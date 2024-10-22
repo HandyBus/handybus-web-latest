deff8f42b18876833cf5e9615a160d6d22420180
Geon <geonulim@gmail.com>
2024-10-22 16:19:29 +0900

    chore: pretendard 폰트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
+import '@/app/fonts/pretendard/font.css';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
