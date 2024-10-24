66253440e31b4b0e557620997d97228a28194c97
Minseok Choe <choems@outlook.com>
2024-10-24 10:55:31 +0900

    design: rating

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -14,40 +14,16 @@ const Rating = ({ value = 0, onChange }: Props) => {
   };
 
   return (
-    <span className="*:pr-1 inline-flex [&>*:last-child]:pr-0">
-      <span
-        className={`relative ${value >= 1 ? 'text-primary-main' : 'text-grey-100'}`}
-      >
-        <div className="absolute h-full min-w-12" onClick={handleClick(0)} />
-        <span onClick={handleClick(1)}>
+    <div className="inline-flex *:pl-[2px] *:pr-[2px] [&>*:first-child]:pl-0 [&>*:last-child]:pr-0">
+      {[1, 2, 3, 4, 5].flatMap((n) => (
+        <span
+          className={`${value >= n ? 'text-primary-main' : 'text-grey-100'}`}
+          onClick={handleClick(2)}
+        >
           <Star />
         </span>
-      </span>
-      <span
-        className={`${value >= 2 ? 'text-primary-main' : 'text-grey-100'}`}
-        onClick={handleClick(2)}
-      >
-        <Star />
-      </span>
-      <span
-        className={`${value >= 3 ? 'text-primary-main' : 'text-grey-100'}`}
-        onClick={handleClick(3)}
-      >
-        <Star />
-      </span>
-      <span
-        className={`${value >= 4 ? 'text-primary-main' : 'text-grey-100'}`}
-        onClick={handleClick(4)}
-      >
-        <Star />
-      </span>
-      <span
-        className={`${value >= 5 ? 'text-primary-main' : 'text-grey-100'}`}
-        onClick={handleClick(5)}
-      >
-        <Star />
-      </span>
-    </span>
+      ))}
+    </div>
   );
 };
 
