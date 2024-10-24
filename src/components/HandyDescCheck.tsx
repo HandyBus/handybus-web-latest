5f2c99a120fc14ca741c1db91c4b003acd641616
Minseok Choe <choems@outlook.com>
2024-10-24 11:10:40 +0900

    feat: handy-description-check

---
+++ src/components/HandyDescCheck.tsx
@@ -0,0 +1,22 @@
+import type { PropsWithChildren } from 'react';
+
+import HandyDescCheckIcon from '@/icons/handy-desc-check.svg';
+
+interface Props extends PropsWithChildren {
+  disabled?: boolean;
+}
+
+const HandyDescCheck = ({ disabled, children }: Props) => {
+  return (
+    <div className="flex flex-row items-center gap-[10px] rounded-lg bg-grey-50 px-16">
+      <span
+        className={`text-20 ${disabled ? 'text-grey-600-sub' : 'text-primary-main'}`}
+      >
+        <HandyDescCheckIcon />
+      </span>
+      <span className="my-12">{children}</span>
+    </div>
+  );
+};
+
+export default HandyDescCheck;
