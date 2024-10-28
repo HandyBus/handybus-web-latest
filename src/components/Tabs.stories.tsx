e36c8076fae6c75a8ca3e12f0307d119a27ca75d
Minseok Choe <choems@outlook.com>
2024-10-28 19:37:43 +0900

    docs(디자인시스템): tabs

---
+++ src/components/Tabs.stories.tsx
@@ -0,0 +1,42 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { useState } from '@storybook/preview-api';
+import Tabs from './Tabs';
+
+const meta: Meta<typeof Tabs> = {
+  title: 'Components/Tabs',
+  component: Tabs,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Tabs>;
+
+export const Static: Story = {
+  args: {
+    items: [
+      { value: 10, label: '예약 내역' },
+      { value: 20, label: '수요 확인 내역' },
+      { value: 30, label: '지난 콘서트' },
+    ],
+    selected: 30,
+    onSelect: undefined,
+  },
+};
+
+export const Interactice: Story = {
+  args: {},
+  render: function () {
+    const items = [
+      { value: 10, label: '예약 내역' },
+      { value: 20, label: '수요 확인 내역' },
+      { value: 30, label: '지난 콘서트' },
+    ];
+    const [selected, setSelected] = useState<number>(30);
+    return Tabs<number>({
+      items: items,
+      selected: selected,
+      onSelect: setSelected,
+    });
+  },
+};
