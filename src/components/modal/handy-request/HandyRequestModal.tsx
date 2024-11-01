53888609b5e2dd95afd810c1fee2a19f8125f59d
전준성 Jun-seong Jeon <87654307+joonseong11@users.noreply.github.com>
2024-11-01 17:58:42 +0900

    feat(디자인시스템): 핸디에게 물어보기 모달 (#31)
    * feat(디자인시스템): 공통 모달 개발
    
    * feat(디자인시스템): 컨펌 모달 구현
    
    * feat(디자인시스템): 노선 선택 모달 구현
    
    * feat(디자인시스템): 핸디에게 물어보기 모달 구현
    
    * fix: storybook 에서 public 경로 이미지 참조 안되던 이슈 해결
    
    * fix: 지워진 컨펌모달 코드 복구
    
    * style: 주석제거
    
    * fix: 지워진 코드 복구
    
    * remove: 사용하지않는 이미지파일 제거

---
+++ src/components/modal/handy-request/HandyRequestModal.tsx
@@ -0,0 +1,66 @@
+import Image from 'next/image';
+import CustomModal from '../CustomModal';
+import Button from '../../buttons/button/Button';
+
+interface Props {
+  onConfirm: () => void;
+  isOpen: boolean;
+  onClosed: () => void;
+}
+
+const HandyRequestModal = ({ onConfirm, isOpen, onClosed }: Props) => {
+  return (
+    <CustomModal
+      isOpen={isOpen}
+      onClosed={onClosed}
+      styles="z-50 flex h-[393px] w-280 flex-col items-center justify-center gap-12 bg-white px-24 py-20 rounded-[20px]"
+    >
+      <ModalContent onConfirm={onConfirm} onClosed={onClosed} />
+    </CustomModal>
+  );
+};
+
+export default HandyRequestModal;
+
+interface ModalContentProps {
+  onConfirm: () => void;
+  onClosed: () => void;
+}
+
+const ModalContent = ({ onConfirm, onClosed }: ModalContentProps) => {
+  return (
+    <>
+      <Image
+        src={'/rectangle.png'}
+        width={40}
+        height={40}
+        alt="Handy Request icon"
+      />
+      <h2
+        className="text-center text-22 font-700 leading-[30.8px]"
+        id="modal-title"
+      >
+        잠시만요! <br />
+        핸디버스의 <span className="text-primary-main">
+          든든한 핸디
+        </span> <br /> 하실 생각 있으세요?
+      </h2>
+      <p
+        id="modal-description"
+        className="text-center text-16 font-400 leading-6 text-grey-500"
+      >
+        핸디버스를 완벽히 합리적으로 <br />
+        이용할 수 있는 기회에요.
+        <br /> 안전한 운행을 도와주면, <br /> 다음 이용료가 절반!
+      </p>
+      <div className="flex w-[100%] flex-col gap-8">
+        <Button variant="primary" onClick={onConfirm}>
+          핸디 역할 알아보기
+        </Button>
+        <Button variant="secondary" onClick={onClosed}>
+          아니요, 괜찮아요
+        </Button>
+      </div>
+    </>
+  );
+};
