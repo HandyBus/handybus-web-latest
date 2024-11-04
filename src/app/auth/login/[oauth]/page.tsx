6e7d0de19b168ad3743be42b9dac79d3fbb7cafd
Geon Woo Lim <geonulim@gmail.com>
2024-11-04 11:32:50 +0900

    feat(로그인): 로그인 페이지 및 소셜 로그인 기능 구현 (#29)
    * feat: 로그인 퍼블리싱
    
    * feat: oauth 리다이렉트 구현
    
    * feat: 카카오 소셜 로그인 구현
    
    * fix: 로그인 중복 요청 방지
    
    * feat: 토큰 관리 로직 구현
    
    * fix: oauth url을 prop으로 받도록 수정

---
+++ src/app/auth/login/[oauth]/page.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { postLogin } from '@/services/auth';
+import { setAccessToken, setRefreshToken } from '@/utils/handleToken';
+import { useRouter } from 'next/navigation';
+import { useEffect, useRef } from 'react';
+
+interface Props {
+  params: { oauth: 'kakao' | 'naver' };
+  searchParams: { code: string; state?: string };
+}
+
+const OAuth = ({ params, searchParams }: Props) => {
+  const router = useRouter();
+  const isInitiated = useRef(false);
+
+  const handleOAuth = async () => {
+    try {
+      const tokens = await postLogin(params.oauth, {
+        code: searchParams.code,
+        state: searchParams?.state,
+      });
+      setRefreshToken(tokens.refreshToken);
+      setAccessToken(tokens.accessToken);
+      router.push('/');
+    } catch (e) {
+      console.error(e);
+      router.push('/login');
+    }
+  };
+
+  useEffect(() => {
+    if (isInitiated.current) {
+      return;
+    }
+    isInitiated.current = true;
+    handleOAuth();
+  }, []);
+
+  return <div />;
+};
+
+export default OAuth;
---
+++ src/app/login/page.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import Logo from 'public/icons/logo-large-white.svg';
+import Kakao from 'public/icons/kakao.svg';
+import Naver from 'public/icons/naver.svg';
+import Link from 'next/link';
+import { OAUTH } from '@/constants/oauth';
+
+const Login = () => {
+  return (
+    <main className="flex h-full w-full flex-col items-center bg-primary-main">
+      <div className="my-auto flex flex-col items-center">
+        <span className="text-18 font-500 text-white">집부터 콘서트장까지</span>
+        <span className="mb-32 text-28 font-700 text-white">
+          핸디버스와 함께
+        </span>
+        <Logo />
+      </div>
+      <div className="mb-16 flex flex-col gap-8">
+        <Link
+          href={OAUTH.kakao()}
+          className="flex h-52 w-320 items-center justify-center gap-16 rounded-full bg-[#FEE500] text-16 font-600 text-black/85"
+        >
+          <Kakao />
+          카카오로 로그인
+        </Link>
+        <Link
+          href={OAUTH.naver()}
+          className="flex h-52 w-320 items-center justify-center gap-16 rounded-full bg-[#03C75A] text-16 font-600 text-white"
+        >
+          <Naver />
+          네이버로 로그인
+        </Link>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
