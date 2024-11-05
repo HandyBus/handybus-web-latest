da0a187dc21de2276ccd3af3eefaf9d72be95a5b
Minseok Choe <choems@outlook.com>
2024-11-05 14:37:59 +0900

    fix: make app-bar general

--- src/components/app-bar/AppBar.tsx
+++ src/components/app-bar/AppBar.tsx
@@ -1,9 +1,10 @@
 'use client';
 
+import type { PropsWithChildren } from 'react';
 import { useRouter } from 'next/navigation';
 import BackIcon from 'public/icons/chevron-left.svg';
 
-const AppBar = () => {
+const AppBar = ({ children }: PropsWithChildren) => {
   const router = useRouter();
   const handleBack = () => {
     router.back();
@@ -14,7 +15,7 @@ const AppBar = () => {
       <button onClick={handleBack}>
         <BackIcon />
       </button>
-      <h1 className="ml-8 text-14 font-600 text-grey-500">회원가입</h1>
+      <h1 className="ml-8 text-14 font-600 text-grey-500">{children}</h1>
     </div>
   );
 };
