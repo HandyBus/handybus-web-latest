f24dd87890f0c15ab2614ea021b141524f6936d3
Geon <geonulim@gmail.com>
2024-11-06 16:54:34 +0900

    feat: 바텀시트 프레임 구현

---
+++ src/components/bottom-sheet/BottomSheet.tsx
@@ -0,0 +1,31 @@
+import { ReactNode, forwardRef } from 'react';
+
+interface Props {
+  children: ReactNode;
+  title?: string;
+}
+
+const BottomSheet = forwardRef<HTMLDivElement, Props>(
+  ({ children, title }, ref) => {
+    return (
+      <div className="fixed bottom-0 left-0 right-0 top-0 bg-black/50">
+        <div
+          ref={ref}
+          role="dialog"
+          className="fixed bottom-0 left-0 right-0 min-h-100 rounded-t-[20px] bg-white px-32"
+        >
+          <div className="mx-auto my-8 h-4 w-[70px] shrink-0 rounded-full bg-grey-100" />
+          {title && (
+            <h2 className="w-full py-12 text-22 font-700 text-grey-900">
+              {title}
+            </h2>
+          )}
+          {children}
+        </div>
+      </div>
+    );
+  },
+);
+BottomSheet.displayName = 'BottomSheet';
+
+export default BottomSheet;
