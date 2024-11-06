278b44c895cfd48d9fefd4539f77ec85105181c0
Geon <geonulim@gmail.com>
2024-11-06 17:32:00 +0900

    feat: 바텀시트 열고 닫기 구현

--- src/components/bottom-sheet/BottomSheet.tsx
+++ src/components/bottom-sheet/BottomSheet.tsx
@@ -8,11 +8,11 @@ interface Props {
 const BottomSheet = forwardRef<HTMLDivElement, Props>(
   ({ children, title }, ref) => {
     return (
-      <div className="fixed bottom-0 left-0 right-0 top-0 bg-black/50">
+      <div className="fixed bottom-0 left-0 right-0 top-0 hidden bg-black/50">
         <div
           ref={ref}
           role="dialog"
-          className="fixed bottom-0 left-0 right-0 min-h-100 rounded-t-[20px] bg-white px-32"
+          className="fixed bottom-0 left-0 right-0 z-[100] hidden min-h-100 rounded-t-[20px] bg-white px-32 transition-transform duration-0 ease-out"
         >
           <div className="mx-auto my-8 h-4 w-[70px] shrink-0 rounded-full bg-grey-100" />
           {title && (
---
+++ src/hooks/useBottomSheet.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useRef } from 'react';
+
+const TRANSFORM_DURATION = '150ms';
+
+const useBottomSheet = () => {
+  const bottomSheetRef = useRef<HTMLDivElement>(null);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  const openBottomSheet = () => {
+    const bottomSheetElement = bottomSheetRef.current;
+    const backdropElement = bottomSheetRef.current?.parentElement;
+    if (!bottomSheetElement || !backdropElement) {
+      return;
+    }
+    bottomSheetElement.style.transitionDuration = '0ms';
+    bottomSheetElement.style.display = 'block';
+    backdropElement.style.display = 'block';
+
+    const bottomSheetHeight = bottomSheetRef.current.clientHeight;
+    bottomSheetElement.style.transform = `translateY(${bottomSheetHeight}px)`;
+
+    setTimeout(() => {
+      bottomSheetElement.style.transitionDuration = TRANSFORM_DURATION;
+      bottomSheetElement.style.transform = `translateY(0px)`;
+    }, 10);
+  };
+
+  const closeBottomSheet = () => {
+    const bottomSheetElement = bottomSheetRef.current;
+    const backdropElement = bottomSheetRef.current?.parentElement;
+    if (!bottomSheetElement || !backdropElement) {
+      return;
+    }
+    const bottomSheetHeight = bottomSheetRef.current.clientHeight;
+    bottomSheetElement.style.transitionDuration = TRANSFORM_DURATION;
+    bottomSheetElement.style.transform = `translateY(${bottomSheetHeight}px)`;
+
+    setTimeout(() => {
+      bottomSheetElement.style.display = 'none';
+      backdropElement.style.display = 'none';
+    }, 300);
+  };
+
+  useEffect(() => {
+    const backdropElement = bottomSheetRef.current?.parentElement;
+    if (!backdropElement) {
+      return;
+    }
+    backdropElement.addEventListener('click', closeBottomSheet);
+    return () => {
+      backdropElement.removeEventListener('click', closeBottomSheet);
+    };
+  }, []);
+
+  return { bottomSheetRef, contentRef, openBottomSheet, closeBottomSheet };
+};
+
+export default useBottomSheet;
