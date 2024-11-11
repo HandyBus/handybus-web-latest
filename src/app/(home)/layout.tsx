752e84a7e4ef74a4d55179e1d57e872125496fb2
Minseok Choe <choems@outlook.com>
2024-11-11 21:01:20 +0900

    refactor: use slots instead

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,13 +1,29 @@
+import type { ReactNode } from 'react';
+import Header from '@/components/header/Header';
 import Footer from '@/components/footer/Footer';
 
+interface Props {
+  demands: ReactNode;
+  reservations: ReactNode;
+  top: ReactNode;
+  bot: ReactNode;
+}
+
 export default function WithFooterLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+  demands,
+  top,
+  bot,
+  reservations,
+}: Readonly<Props>) {
   return (
     <div className="flex h-full flex-grow flex-col">
-      {children}
+      <Header />
+      <main className="w-full flex-grow pb-12">
+        {top}
+        {reservations}
+        {demands}
+        {bot}
+      </main>
       <Footer />
     </div>
   );
