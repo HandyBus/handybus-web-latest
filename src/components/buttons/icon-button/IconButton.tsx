60da94f0d454c582e5124f48e5fec1ba8d5d26b3
jujeon <jwjsjwnsstjd@gmail.com>
2024-11-12 17:24:09 +0900

    fix(디자인시스템): icon button의 style 추가 반영할 수 있게 변경

--- src/components/buttons/icon-button/IconButton.tsx
+++ src/components/buttons/icon-button/IconButton.tsx
@@ -2,17 +2,18 @@ import type { HTMLProps } from 'react';
 
 interface Props extends HTMLProps<HTMLButtonElement> {
   type?: 'button' | 'submit' | 'reset';
+  className?: string;
 }
 
-const IconButton = ({ children, disabled, ...rest }: Props) => {
+const IconButton = ({ children, disabled, className, ...rest }: Props) => {
   return (
     <button
       disabled={disabled}
       className={`
-        flex aspect-square h-28 w-28 flex-row
+        flex aspect-square flex-row
         items-center justify-center whitespace-nowrap
         rounded-full bg-grey-50 text-18 text-grey-700
-        active:bg-grey-100 disabled:cursor-not-allowed disabled:bg-grey-50 disabled:text-grey-300
+        active:bg-grey-100 disabled:cursor-not-allowed disabled:bg-grey-50 disabled:text-grey-300 ${className}
       `}
       {...rest}
     >
