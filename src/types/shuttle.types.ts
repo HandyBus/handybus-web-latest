4da1551113765f3f704b6468d1dc103ba7b99b68
Minseok Choe <choems@outlook.com>
2024-11-18 20:48:19 +0900

    fix: type

--- src/types/shuttle.types.ts
+++ src/types/shuttle.types.ts
@@ -17,3 +17,74 @@ export const SECTION = {
 } as const;
 
 export type SectionType = (typeof SECTION)[keyof typeof SECTION];
+export interface ShuttleRoute {
+  shuttleRouteID: number;
+  dailyShuttleID: number;
+  shuttle: ShuttleRouteEvent;
+  name: string;
+  status: 'OPEN' | 'CLOSED' | 'CONFIRMED' | 'ENDED' | 'CANCELLED' | 'INACTIVE';
+  hasEarlyBird: boolean;
+  earlybirdDeadline: string;
+  reservationDeadline: string;
+  earlybirdPriceToDestination: number;
+  earlybirdPriceFromDestination: number;
+  earlybirdPriceRoundTrip: number;
+  regularPriceToDestination: number;
+  regularPriceFromDestination: number;
+  regularPriceRoundTrip: number;
+  maxPassengerCount: number;
+  remainingSeatCount: number;
+  remainingSeatType: 'TO_DESTINATION' | 'FROM_DESTINATION' | 'ROUND_TRIP';
+  hubs: Hub;
+}
+
+export interface Hub {
+  pickup: {
+    name: string;
+    sequence: number;
+    regionID: number;
+    arrivalTime: string;
+  }[];
+
+  dropoff: {
+    name: string;
+    sequence: number;
+    regionID: number;
+    arrivalTime: string;
+  }[];
+
+  destination: {
+    name: string;
+    sequence: number;
+    regionID: number;
+    arrivalTime: string;
+  };
+}
+
+export interface ShuttleRouteEvent {
+  name: string;
+  dailyShuttles: {
+    name: string;
+    longitude: number;
+    latitude: number;
+  }[];
+  image: string;
+  status: 'OPEN' | 'ENDED' | 'INACTIVE';
+  destination: {
+    name: string;
+    longitude: number;
+    latitude: number;
+  };
+  type: 'CONCERT' | 'FESTIVAL';
+  participants: {
+    name: string;
+    longitude: number;
+    latitude: number;
+  }[];
+}
+
+export interface DailyShuttleDetailProps {
+  id: number;
+  date: string;
+  status: 'OPEN' | 'CLOSED' | 'ENDED' | 'INACTIVE';
+}
