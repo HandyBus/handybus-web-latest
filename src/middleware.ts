7aef865da13217a13284238c5ac58fd9922ae717
Geon <geonulim@gmail.com>
2024-11-26 12:41:03 +0900

    fix: 인증이 필요한 페이지 처리 로직 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,12 +6,24 @@ import { AxiosError } from 'axios';
 const REFRESH_TOKEN = 'refresh-token';
 const ACCESS_TOKEN = 'access-token';
 
+const AuthRequiredPages = ['/mypage'];
+
 export const middleware = async (req: NextRequest) => {
+  let accessToken = req.cookies.get(ACCESS_TOKEN)?.value ?? '';
+
+  // 인증이 필요한 페이지인 경우
+  if (AuthRequiredPages.includes(req.nextUrl.pathname)) {
+    if (!accessToken) {
+      return redirectToLogin(req.url);
+    } else {
+      return NextResponse.next();
+    }
+  }
+
+  // API 요청인 경우
   const url = BASE_URL + req.nextUrl.href.split('/api/')[1];
   req.nextUrl.href = url;
-  const response = NextResponse.redirect(req.nextUrl);
-
-  let accessToken = req.cookies.get(ACCESS_TOKEN)?.value ?? '';
+  const response = NextResponse.rewrite(req.nextUrl);
 
   if (!accessToken) {
     try {
@@ -22,23 +34,21 @@ export const middleware = async (req: NextRequest) => {
       const newTokens = await postRefreshToken(refreshToken);
       accessToken = newTokens.accessToken;
       response.cookies.set(REFRESH_TOKEN, newTokens.refreshToken);
+      response.cookies.set(ACCESS_TOKEN, newTokens.refreshToken);
     } catch (e) {
       const error = e as AxiosError;
       if (error.status === 401) {
         return redirectToLogin(req.url);
       }
-      console.error(error);
     }
   }
 
   response.headers.set('Authorization', `Bearer ${accessToken}`);
-  response.cookies.set(ACCESS_TOKEN, accessToken);
-
   return response;
 };
 
 export const config = {
-  matcher: ['/api/:path*'],
+  matcher: ['/api/:path*', '/mypage'],
 };
 
 const redirectToLogin = (url: string) => {
