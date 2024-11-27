179d67d3bb5af8c7ab16df47b77f20d9797fbf58
Geon <geonulim@gmail.com>
2024-11-27 11:35:10 +0900

    chore: 불필요한 코드 삭제

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,14 +8,11 @@ export const middleware = async (req: NextRequest) => {
   const session = req.cookies.get(SESSION)?.value;
   const parsedSession = JSON.parse(session ?? '{}') as SessionType;
 
-  console.log('MIDDLEWARE: ', parsedSession.isLoggedIn, req.url);
-
   // 인증 필요 페이지에 접근할 때 세션이 없으면 로그인 페이지로 리다이렉트
   if (
     AuthRequiredPages.includes(req.nextUrl.pathname) &&
     !parsedSession.isLoggedIn
   ) {
-    console.log('DELETE TOKEN');
     const response = NextResponse.rewrite(new URL('/login', req.url));
     response.cookies.delete(REFRESH_TOKEN);
     response.cookies.delete(ACCESS_TOKEN);
