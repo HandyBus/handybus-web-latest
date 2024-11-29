7008435c3bfcb634a5ff782813ccf4d60b762d5b
Minseok Choe <choems@outlook.com>
2024-11-29 10:43:16 +0900

    feat(홈): 수요 확인 중인 셔틀 컴포넌트 및 상세 보기 페이지 (#69)
    * feat: demand page
    
    * fix: type
    
    * fix: use new api
    
    * docs: fix comment
    
    * docs: remove comment
    
    * fix: api url
    
    * feat: use ssr
    
    * fix: types

---
+++ src/app/(home)/@demand/page.tsx
@@ -0,0 +1,36 @@
+import Article from '@/components/article/Article';
+import RedirectButton from '@/components/buttons/redirect-button/RedirectButton';
+import DemandView from './components/DemandView';
+import dynamic from 'next/dynamic';
+const Empty = dynamic(() => import('@/app/demand/components/Empty'));
+import { getOpenDemandings } from '@/app/demand/utils/fetch.util';
+
+// TODO check urn : /demand-survey
+const Page = () => (
+  <Article richTitle="수요 확인 중인 셔틀" showMore="/demand">
+    <SubPage />
+    <div className="w-full p-16">
+      <RedirectButton description="찾고 있는 셔틀이 없나요?" href="/TODO">
+        원하는 셔틀 요청하기
+      </RedirectButton>
+    </div>
+  </Article>
+);
+
+export default Page;
+
+const SubPage = async () => {
+  const data = await getOpenDemandings();
+
+  if (data.length === 0) {
+    return <Empty />;
+  }
+
+  return (
+    <div className="flex flex-col">
+      {data.map((d) => (
+        <DemandView key={d.id} event={d} />
+      ))}
+    </div>
+  );
+};
