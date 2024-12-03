3a4080685ab43295c1c9ec12980f85d2b4f6bc54
Geon <geonulim@gmail.com>
2024-12-03 14:14:15 +0900

    feat: 리뷰 폼 제출 구현

--- src/app/mypage/reviews/write/[id]/page.tsx
+++ src/app/mypage/reviews/write/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import AppBar from '@/components/app-bar/AppBar';
 import Rating from '@/components/rating/Rating';
-import { ChangeEvent, useRef, useState } from 'react';
+import { ChangeEvent, FormEvent, useRef, useState } from 'react';
 import CameraIcon from 'public/icons/camera-black.svg';
 import Button from '@/components/buttons/button/Button';
 import { toast } from 'react-toastify';
@@ -12,6 +12,7 @@ import XIcon from 'public/icons/x.svg';
 
 const WriteReview = () => {
   const [rating, setRating] = useState(0);
+  const [text, setText] = useState('');
   const [files, setFiles] = useState<File[]>([]);
 
   const fileInputRef = useRef<HTMLInputElement>(null);
@@ -36,10 +37,19 @@ const WriteReview = () => {
     setFiles((prev) => prev.filter((f) => f !== file));
   };
 
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (text.length < 20) {
+      toast.error('20자 이상 작성해주세요.');
+      return;
+    }
+    console.log(rating, text, files);
+  };
+
   return (
     <>
       <AppBar>후기 작성</AppBar>
-      <form className="relative grow">
+      <form onSubmit={handleSubmit} className="relative grow">
         <div className="h-100 w-full bg-grey-200">Shuttle Card</div>
         <section className="flex w-full flex-col gap-16 p-28">
           <h5>이번 셔틀의 만족도를 입력해주세요</h5>
@@ -50,6 +60,8 @@ const WriteReview = () => {
         <section className="flex w-full flex-col gap-16 p-28">
           <h5>후기를 작성해주세요 (20자 이상)</h5>
           <textarea
+            value={text}
+            onChange={(e) => setText(e.target.value)}
             placeholder="자유로운 후기를 작성해주세요. 핸디버스 발전에 도움이 됩니다. (장단점, 건의사항 등)"
             className="h-160 w-full resize-none rounded-[12px] border border-grey-100 p-12 text-16 font-400 outline-none placeholder:text-grey-300"
           />
