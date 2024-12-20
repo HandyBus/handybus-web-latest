eb3d3c4a4b82fcac52135c545f8220a9a1c34384
전준성 Jun-seong Jeon <87654307+joonseong11@users.noreply.github.com>
2024-12-20 17:09:28 +0900

    feat(수요조사하기): 수요조사신청하기 api 연동 (#106)
    * feat: 수요조사신청하기 api 연동
    
    * style: props에 interface 반영
    
    * style: sections 를 components와 hooks 폴더로 분리

--- src/components/shuttle-detail/components/PassengerCount.tsx
+++ src/components/shuttle-detail/components/PassengerCount.tsx
@@ -1,4 +1,3 @@
-import { Dispatch, SetStateAction } from 'react';
 import Counter from '@/components/counter/Counter';
 
 const PassengerCount = ({
@@ -6,14 +5,19 @@ const PassengerCount = ({
   setCount,
 }: {
   count: number;
-  setCount: Dispatch<SetStateAction<number>>;
+  setCount: (value: number) => void;
 }) => {
   return (
     <section className="flex flex-col gap-[16px] px-16 py-28">
       <h2 className="text-22 font-700 leading-[30.8px] text-grey-900">
         탑승객 수를 입력해주세요 <span className="text-red-500">*</span>
       </h2>
-      <Counter count={count} setCount={setCount} />
+      <Counter
+        count={count}
+        setCount={(newValue: number) => {
+          setCount(newValue);
+        }}
+      />
       <p className="text-12 font-400 leading-[19.2px] text-grey-500">
         10명 이상 예약하는 경우, <u>핸디버스 카카오 채널</u>로 문의 바랍니다.
       </p>
