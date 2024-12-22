83805020ab7908aca7eaafecd8e74d332b09d98b
Geon Woo Lim <geonulim@gmail.com>
2024-12-23 00:34:23 +0900

    feat(마이페이지-셔틀): 예약 상세보기 및 핸디 가이드 페이지 마크업 (#110)
    * design: 예약 셔틀 카드 태그 디자인 추가
    
    * fix: 예약 상세 페이지 변경사항 반영
    
    * feat: 핸디 가이드 페이지 마크업
    
    * feat: 예약 취소 상세보기 마크업

--- src/app/mypage/shuttle/components/Section.tsx
+++ src/app/mypage/shuttle/components/Section.tsx
@@ -10,7 +10,7 @@ const Section = ({ children, title }: Props) => {
   return (
     <>
       <Divider />
-      <section className="px-16 py-32 text-16 font-400 text-grey-900">
+      <section className="w-full px-16 py-32 text-16 font-400 text-grey-900">
         <h3 className="mb-8 line-clamp-1 h-32 text-22 font-700">{title}</h3>
         {children}
       </section>
