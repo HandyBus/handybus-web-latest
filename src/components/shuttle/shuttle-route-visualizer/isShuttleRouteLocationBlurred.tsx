c266e27d6d4bb71d728d81c690820f2b4ef99f0e
jujeon <jwjsjwnsstjd@gmail.com>
2024-12-23 14:27:23 +0900

    refactor: shuttle route visualizer 컴포넌트 구조개선

---
+++ src/components/shuttle/shuttle-route-visualizer/isShuttleRouteLocationBlurred.tsx
@@ -0,0 +1,21 @@
+import { ROUTE_TYPE, ShuttleRouteObject } from '@/types/shuttle.types';
+import { RouteType } from '@/types/shuttle.types';
+
+const isShuttleRouteLocationBlurred = ({
+  object,
+  type,
+  index,
+  length,
+}: {
+  object: ShuttleRouteObject;
+  type: RouteType;
+  index: number;
+  length: number;
+}) => {
+  if (type === ROUTE_TYPE.DEPARTURE)
+    return !(index === length - 1 || object.isPickup);
+  if (type === ROUTE_TYPE.RETURN) return !(index === 0 || object.isDropoff);
+  return false;
+};
+
+export default isShuttleRouteLocationBlurred;
