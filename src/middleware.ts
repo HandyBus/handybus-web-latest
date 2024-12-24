e989c8c5b4d5e2853e494c2431ba6a1abeaaee77
Geon Woo Lim <geonulim@gmail.com>
2024-12-24 14:51:01 +0900

    fix: 미들웨어에서 access token 만료 핸들링 로직 추가 (#117)

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,44 +13,34 @@ export const middleware = async (req: NextRequest) => {
 
   // 토큰 만료 시 토큰 재발급
   if (refreshToken && !accessToken) {
-    try {
-      const response = NextResponse.redirect(new URL(req.url));
-      const tokens = await updateToken();
-      response.cookies.set(ACCESS_TOKEN, tokens.accessToken, {
-        ...OPTIONS,
-        expires: new Date(tokens.accessTokenExpiresAt),
-      });
-      response.cookies.set(REFRESH_TOKEN, tokens.refreshToken, {
-        ...OPTIONS,
-        expires: new Date(tokens.refreshTokenExpiresAt),
-      });
-      return response;
-    } catch (error) {
-      console.error('미들웨어 로그인 시간 만료: ', error);
-      const response = NextResponse.redirect(new URL('/login', req.url));
-      response.cookies.delete(ACCESS_TOKEN);
-      response.cookies.delete(REFRESH_TOKEN);
-      return response;
-    }
+    return handleRefreshToken(req);
   }
 
   // 로그인 여부에 따라 리다이렉트
   if (!refreshToken) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
-  const progress = await getProgress();
-  if (
-    progress !== 'ONBOARDING_COMPLETE' &&
-    req.nextUrl.pathname !== '/onboarding'
-  ) {
-    return NextResponse.redirect(new URL('/onboarding', req.url));
+  try {
+    const progress = await getProgress();
+    if (
+      progress !== 'ONBOARDING_COMPLETE' &&
+      req.nextUrl.pathname !== '/onboarding'
+    ) {
+      return NextResponse.redirect(new URL('/onboarding', req.url));
+    }
+  } catch {
+    return handleRefreshToken(req);
   }
 
   // 온보딩을 완료하고 온보딩 페이지 접근 시 마이페이지로 리다이렉트
   if (req.nextUrl.pathname === '/onboarding' && refreshToken) {
-    const progress = await getProgress();
-    if (progress === 'ONBOARDING_COMPLETE') {
-      return NextResponse.redirect(new URL('/mypage', req.url));
+    try {
+      const progress = await getProgress();
+      if (progress === 'ONBOARDING_COMPLETE') {
+        return NextResponse.redirect(new URL('/mypage', req.url));
+      }
+    } catch {
+      return handleRefreshToken(req);
     }
   }
 
@@ -59,4 +49,26 @@ export const middleware = async (req: NextRequest) => {
 
 export const config = {
   matcher: ['/onboarding', '/mypage/:path*', '/demand/:path*/write'],
+};
+
+const handleRefreshToken = async (req: NextRequest) => {
+  try {
+    const response = NextResponse.redirect(new URL(req.url));
+    const tokens = await updateToken();
+    response.cookies.set(ACCESS_TOKEN, tokens.accessToken, {
+      ...OPTIONS,
+      expires: new Date(tokens.accessTokenExpiresAt),
+    });
+    response.cookies.set(REFRESH_TOKEN, tokens.refreshToken, {
+      ...OPTIONS,
+      expires: new Date(tokens.refreshTokenExpiresAt),
+    });
+    return response;
+  } catch (error) {
+    console.error('미들웨어 로그인 시간 만료: ', error);
+    const response = NextResponse.redirect(new URL('/login', req.url));
+    response.cookies.delete(ACCESS_TOKEN);
+    response.cookies.delete(REFRESH_TOKEN);
+    return response;
+  }
 };
