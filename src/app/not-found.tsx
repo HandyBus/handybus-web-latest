8ad945bf79e4aca46e4d184a737f57942f00c525
Geon <geonulim@gmail.com>
2024-12-24 15:48:29 +0900

    feat: not found 페이지 구현

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,15 +4,7 @@ import Footer from '@/components/footer/Footer';
 import { revalidatePaths } from '@/utils/revalidatePath';
 import Image from 'next/image';
 
-export const Error = ({
-  error,
-  reset,
-}: {
-  error: Error & { digest?: string };
-  reset: () => void;
-}) => {
-  console.error(error);
-
+export const NotFound = () => {
   const handleLogout = async () => {
     try {
       const response = await fetch('/api/logout', {
@@ -31,11 +23,10 @@ export const Error = ({
   return (
     <div className="flex grow flex-col px-20 py-28">
       <h2 className="pb-[10px] text-26 font-700 text-grey-900">
-        알 수 없는 오류가 발생했어요
+        요청하신 페이지를 찾지 못했어요
       </h2>
       <p className="pb-28 text-14 font-600 text-grey-500">
-        홈으로 돌아가거나 입력하신 주소가 맞는지 확인해주세요. 오류가 반복되면
-        하단 ‘카카오톡 문의하기’를 통해 문의를 남겨주세요.
+        홈으로 돌아가거나 입력하신 주소가 맞는지 확인해주세요.
       </p>
       <div className="relative mb-40 h-200 w-full overflow-hidden rounded-[12px]">
         <Image
@@ -45,12 +36,6 @@ export const Error = ({
           className="aspect-auto object-cover"
         />
       </div>
-      <button
-        className="mb-20 h-44 w-full rounded-full bg-grey-50 text-16 font-400 text-grey-700"
-        onClick={() => reset()}
-      >
-        다시 시도하기
-      </button>
       <button
         className="h-44 w-full rounded-full bg-grey-50 text-16 font-400 text-grey-700"
         onClick={() => handleLogout()}
@@ -63,4 +48,4 @@ export const Error = ({
   );
 };
 
-export default Error;
+export default NotFound;
