b510afb97490804af46a73c634f316063548f98c
Geon <geonulim@gmail.com>
2024-12-24 11:27:16 +0900

    feat: 기본 메타데이터 추가

---
+++ src/constants/metadata.ts
@@ -0,0 +1,4 @@
+export const TITLE = '핸디버스';
+export const DESCRIPTION =
+  '공연/행사 수요응답형 모빌리티 플랫폼, 핸디버스를 통해 내 지역에서부터 목적지까지 "내가 원하는" 셔틀을 이용할 수 있어요. 콘서트, 공연, 행사 등 다양한 이동의 여정에서 핸디버스를 만나보세요!';
+export const URL = 'https://www.handybus.co.kr';
