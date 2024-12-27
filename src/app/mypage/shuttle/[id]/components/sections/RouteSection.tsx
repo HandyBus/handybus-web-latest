2b0639be8b647431c77d597f573bb194cfb37242
jujeon <jwjsjwnsstjd@gmail.com>
2024-12-27 12:22:42 +0900

    feat: shuttle visualizer api response 에 맞게 업데이트, 노선 타입 추가

--- src/app/mypage/shuttle/[id]/components/sections/RouteSection.tsx
+++ src/app/mypage/shuttle/[id]/components/sections/RouteSection.tsx
@@ -1,20 +1,14 @@
 import ShuttleRouteVisualizer from '@/components/shuttle/shuttle-route-visualizer/ShuttleRouteVisualizer';
 import Divider from '../Divider';
-import { SECTION } from '@/types/shuttle.types';
+import { SECTION, ShuttleRouteHubObject } from '@/types/shuttle.types';
 
 const RouteSection = () => {
   return (
     <>
       <Divider />
       <ShuttleRouteVisualizer
-        object={[
-          { time: '2024-03-20 14:30:00', hubName: '청주터미널', hubId: '1' },
-          { time: '2024-03-20 14:40:00', hubName: '청주대학교', hubId: '2' },
-          { time: '2024-03-20 14:50:00', hubName: '장소3', hubId: '3' },
-          { time: '2024-03-20 15:00:00', hubName: '장소4', hubId: '4' },
-          { time: '2024-03-20 15:10:00', hubName: '장소5', hubId: '5' },
-          { time: '2024-03-20 15:20:00', hubName: '장소6', hubId: '6' },
-        ]}
+        toDestinationObject={mockData}
+        fromDestinationObject={mockData}
         section={SECTION.MY_RESERVATION}
       />
     </>
@@ -22,3 +16,42 @@ const RouteSection = () => {
 };
 
 export default RouteSection;
+
+const mockData = [
+  {
+    arrivalTime: '2024-03-20 14:30:00',
+    name: '청주터미널',
+    shuttleRouteHubId: 1,
+    sequence: 1,
+  },
+  {
+    arrivalTime: '2024-03-20 14:40:00',
+    name: '청주대학교',
+    shuttleRouteHubId: 2,
+    sequence: 2,
+  },
+  {
+    arrivalTime: '2024-03-20 14:50:00',
+    name: '장소3',
+    shuttleRouteHubId: 3,
+    sequence: 3,
+  },
+  {
+    arrivalTime: '2024-03-20 15:00:00',
+    name: '장소4',
+    shuttleRouteHubId: 4,
+    sequence: 4,
+  },
+  {
+    arrivalTime: '2024-03-20 15:10:00',
+    name: '장소5',
+    shuttleRouteHubId: 5,
+    sequence: 5,
+  },
+  {
+    arrivalTime: '2024-03-20 15:20:00',
+    name: '장소6',
+    shuttleRouteHubId: 6,
+    sequence: 6,
+  },
+] as ShuttleRouteHubObject[];
