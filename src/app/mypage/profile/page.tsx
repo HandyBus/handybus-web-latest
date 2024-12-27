4be4623ca2b9ef4c2b3ae2f0a044c715ae337664
Geon <geonulim@gmail.com>
2024-12-27 13:17:14 +0900

    fix: 마이페이지 하위 경로들에 csr 및 로딩 ui 적용

--- src/app/mypage/profile/page.tsx
+++ src/app/mypage/profile/page.tsx
@@ -1,18 +1,22 @@
+'use client';
+
 import AppBar from '@/components/app-bar/AppBar';
 import ListButton from '../components/ListButton';
 import Image from 'next/image';
-import { getUserDashboard } from '@/services/users';
+import { useGetUserDashboard } from '@/services/users';
 import { DEFAULT_PROFILE_IMAGE } from '@/constants/common';
 import { ID_TO_REGION } from '@/constants/regions';
+import DeferredSuspense from '@/components/loading/DeferredSuspense';
+import Loading from '@/components/loading/Loading';
 
-const Profile = async () => {
-  const userDashboard = await getUserDashboard();
+const Profile = () => {
+  const { data: userDashboard, isLoading } = useGetUserDashboard();
 
   const nickname = userDashboard?.nickname ?? '';
   const profileImage = userDashboard?.profileImage ?? '';
   const gender = userDashboard?.gender === 'MALE' ? '남성' : '여성';
   const ageRange = userDashboard?.ageRange ?? '';
-  const region = ID_TO_REGION[userDashboard.regionId];
+  const region = ID_TO_REGION[userDashboard?.regionId ?? 1];
   const favoriteArtists =
     userDashboard?.favoriteArtists.map((artist) => artist.name).join(', ') ??
     '';
@@ -20,51 +24,53 @@ const Profile = async () => {
   return (
     <>
       <AppBar>회원 정보 관리</AppBar>
-      <main>
-        <section className="p-16">
-          <div className="flex items-center gap-12">
-            <div className="relative h-40 w-40 overflow-hidden rounded-full">
-              <Image
-                src={profileImage || DEFAULT_PROFILE_IMAGE}
-                alt="프로필 이미지"
-                fill
-                className="object-cover"
-              />
+      <DeferredSuspense fallback={<Loading />} isLoading={isLoading}>
+        <main>
+          <section className="p-16">
+            <div className="flex items-center gap-12">
+              <div className="relative h-40 w-40 overflow-hidden rounded-full">
+                <Image
+                  src={profileImage || DEFAULT_PROFILE_IMAGE}
+                  alt="프로필 이미지"
+                  fill
+                  className="object-cover"
+                />
+              </div>
+              <span className="text-18 font-500 text-grey-900">{nickname}</span>
             </div>
-            <span className="text-18 font-500 text-grey-900">{nickname}</span>
-          </div>
-          <ul className="flex flex-col gap-8 pt-16">
-            <ProfileItem title="성별" description={gender} />
-            <ProfileItem title="연령대" description={ageRange} />
-            <ProfileItem
-              title="거주 지역"
-              description={`${region.bigRegion ?? ''} ${region.smallRegion ?? ''}`}
-            />
-            <ProfileItem title="최애 가수" description={favoriteArtists} />
-          </ul>
-        </section>
-        <div className="h-16 w-full bg-grey-50" />
-        <ListButton
-          title="프로필 수정"
-          href="/mypage/profile/edit?type=profile"
-          replace
-        />
-        <ListButton
-          title="성별 및 연령대 수정"
-          href="/mypage/profile/edit?type=personal-info"
-          replace
-        />
-        <ListButton
-          title="거주 지역 수정"
-          href="/mypage/profile/edit?type=region"
-          replace
-        />
-        <ListButton
-          title="최애 가수 수정"
-          href="/mypage/profile/edit?type=artist"
-          replace
-        />
-      </main>
+            <ul className="flex flex-col gap-8 pt-16">
+              <ProfileItem title="성별" description={gender} />
+              <ProfileItem title="연령대" description={ageRange} />
+              <ProfileItem
+                title="거주 지역"
+                description={`${region.bigRegion ?? ''} ${region.smallRegion ?? ''}`}
+              />
+              <ProfileItem title="최애 가수" description={favoriteArtists} />
+            </ul>
+          </section>
+          <div className="h-16 w-full bg-grey-50" />
+          <ListButton
+            title="프로필 수정"
+            href="/mypage/profile/edit?type=profile"
+            replace
+          />
+          <ListButton
+            title="성별 및 연령대 수정"
+            href="/mypage/profile/edit?type=personal-info"
+            replace
+          />
+          <ListButton
+            title="거주 지역 수정"
+            href="/mypage/profile/edit?type=region"
+            replace
+          />
+          <ListButton
+            title="최애 가수 수정"
+            href="/mypage/profile/edit?type=artist"
+            replace
+          />
+        </main>
+      </DeferredSuspense>
     </>
   );
 };
