8ec31c418ffdd3388b93792a804f469de43fa3d4
Geon <geonulim@gmail.com>
2024-12-31 16:44:24 +0900

    feat: 쿠폰 등록 연결

--- src/app/mypage/coupons/components/RegisterCoupon.tsx
+++ src/app/mypage/coupons/components/RegisterCoupon.tsx
@@ -2,17 +2,25 @@
 
 import Button from '@/components/buttons/button/Button';
 import TextInput from '@/components/inputs/text-input/TextInput';
+import { usePostCoupon } from '@/services/coupon';
 import { useForm } from 'react-hook-form';
 
 const RegisterCoupon = () => {
-  const { control, setValue } = useForm<{ coupon: string }>();
+  const { control, setValue, handleSubmit } = useForm<{ coupon: string }>();
+  const { mutate: postCoupon } = usePostCoupon();
+  const onSubmit = (data: { coupon: string }) => {
+    postCoupon(data.coupon);
+  };
   return (
-    <section className="flex flex-col gap-4 px-16 py-28">
+    <form
+      className="flex flex-col gap-24 px-16 py-28"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <TextInput name="coupon" control={control} setValue={setValue}>
         쿠폰 코드
       </TextInput>
       <Button>쿠폰 등록하기</Button>
-    </section>
+    </form>
   );
 };
 
