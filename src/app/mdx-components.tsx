f788f71dcacce53aa8fe5645a2ec2918950dcc91
Geon <geonulim@gmail.com>
2024-12-31 14:07:09 +0900

    feat: mdx로 약관을 볼 수 있도록 구현

--- src/app/mdx-components.tsx
+++ src/app/mdx-components.tsx
@@ -2,6 +2,21 @@ import type { MDXComponents } from 'mdx/types';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    table: ({ children }) => (
+      <div className="my-8 overflow-x-auto">
+        <table className="min-w-full border-collapse border-spacing-0">
+          {children}
+        </table>
+      </div>
+    ),
+    th: ({ children }) => (
+      <th className="border-gray-300 bg-gray-100 py-2 font-semibold border px-4 text-left">
+        {children}
+      </th>
+    ),
+    td: ({ children }) => (
+      <td className="border-gray-300 py-2 border px-4">{children}</td>
+    ),
     ...components,
   };
 }
---
+++ src/app/policy/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import PolicyViewer from '@/components/policy/PolicyViewer';
+
+const Page = () => {
+  return <PolicyViewer type="서비스이용약관" />;
+};
+
+export default Page;
