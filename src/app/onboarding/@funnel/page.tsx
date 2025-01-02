f068f451e7f1c3062cc3d744a4e112246e0a996f
Geon <geonulim@gmail.com>
2025-01-02 13:43:01 +0900

    fix: 온보딩이 완료된 상태에서 토큰이 존재할 경우 처리

--- src/app/onboarding/@funnel/page.tsx
+++ src/app/onboarding/@funnel/page.tsx
@@ -2,11 +2,13 @@ import { getUser } from '@/services/users';
 import OnboardingFunnel from './components/OnboardingFunnel';
 import { parseProgress } from '@/utils/parseProgress';
 import { redirect } from 'next/navigation';
+import { removeOnboardingToken } from '@/utils/handleToken';
 
 const Funnel = async () => {
   const user = await getUser();
   const progress = parseProgress(user.progresses);
   if (progress === 'ONBOARDING_COMPLETE') {
+    await removeOnboardingToken();
     return redirect('/mypage');
   }
   return (
