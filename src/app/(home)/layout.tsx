ab738dca23edfd96ae84e9bacf5c0963644acab1
Geon <geonulim@gmail.com>
2025-01-08 11:19:51 +0900

    rename: home shuttle을 reservation으로 이름 변경

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -3,24 +3,24 @@ import Header from '@/components/header/Header';
 import Footer from '@/components/footer/Footer';
 
 interface Props {
-  demand: ReactNode;
-  shuttle: ReactNode;
   top: ReactNode;
   bot: ReactNode;
+  reservation: ReactNode;
+  demand: ReactNode;
 }
 
 export default function WithFooterLayout({
-  demand,
   top,
   bot,
-  shuttle,
+  reservation,
+  demand,
 }: Readonly<Props>) {
   return (
     <div className="flex h-full flex-grow flex-col">
       <Header />
       <main className="w-full flex-grow pb-12">
         {top}
-        {shuttle}
+        {reservation}
         {demand}
         {bot}
       </main>
