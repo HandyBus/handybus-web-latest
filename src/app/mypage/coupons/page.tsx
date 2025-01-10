de3c4bc7744bc451b0675bc18f3595dbef4240e0
Geon <geonulim@gmail.com>
2025-01-10 13:03:39 +0900

    fix: 쿠폰 api에 상태 쿼리 반영

--- src/app/mypage/coupons/page.tsx
+++ src/app/mypage/coupons/page.tsx
@@ -1,29 +1,18 @@
-'use client';
-
 import AppBar from '@/components/app-bar/AppBar';
 import CouponList from './components/CouponList';
 import RegisterCoupon from './components/RegisterCoupon';
-import DeferredSuspense from '@/components/loading/DeferredSuspense';
-import Loading from '@/components/loading/Loading';
-import { useGetUserCoupons } from '@/services/coupon';
 
-const Coupons = () => {
-  const { data: coupons, isLoading } = useGetUserCoupons();
-
+const Page = () => {
   return (
     <>
       <AppBar>쿠폰함</AppBar>
-      <DeferredSuspense fallback={<Loading />} isLoading={isLoading}>
-        {coupons && (
-          <main>
-            <RegisterCoupon />
-            <div className="h-8 bg-grey-50" />
-            <CouponList coupons={coupons} />
-          </main>
-        )}
-      </DeferredSuspense>
+      <main className="flex grow flex-col">
+        <RegisterCoupon />
+        <div className="h-8 bg-grey-50" />
+        <CouponList />
+      </main>
     </>
   );
 };
 
-export default Coupons;
+export default Page;
