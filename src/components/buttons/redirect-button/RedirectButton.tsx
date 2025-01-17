d3851d896748a76b5cca832db580dc8ed85f6ca5
Geon <geonulim@gmail.com>
2025-01-17 16:34:13 +0900

    fix: redirect button 타입 수정

--- src/components/buttons/redirect-button/RedirectButton.tsx
+++ src/components/buttons/redirect-button/RedirectButton.tsx
@@ -1,8 +1,9 @@
-import Link, { LinkProps } from 'next/link';
+import Link from 'next/link';
 import ChevronRight from 'public/icons/chevron-right.svg';
 import { ReactNode } from 'react';
 
-interface Props extends LinkProps {
+interface Props {
+  href?: string;
   onClick?: () => void; // for opening bottom sheet
   description: string;
   target?: string;
@@ -12,24 +13,27 @@ interface Props extends LinkProps {
 const RedirectButton = ({
   description,
   children,
+  href,
   onClick,
-  ...props
+  target,
 }: Props) => {
   return (
     <>
-      <Link {...props}>
-        <div className="flex w-full flex-row items-center justify-between gap-16 rounded-[10px] bg-grey-50 p-24">
-          <div className="flex flex-col justify-start gap-4 text-left">
-            <span className="text-12 font-400 text-grey-600-sub">
-              {description}
+      {href && (
+        <Link href={href} target={target}>
+          <div className="flex w-full flex-row items-center justify-between gap-16 rounded-[10px] bg-grey-50 p-24">
+            <div className="flex flex-col justify-start gap-4 text-left">
+              <span className="text-12 font-400 text-grey-600-sub">
+                {description}
+              </span>
+              <span className="text-22 font-500 text-grey-800">{children}</span>
+            </div>
+            <span className="text-24 text-grey-600-sub">
+              <ChevronRight />
             </span>
-            <span className="text-22 font-500 text-grey-800">{children}</span>
           </div>
-          <span className="text-24 text-grey-600-sub">
-            <ChevronRight />
-          </span>
-        </div>
-      </Link>
+        </Link>
+      )}
       {onClick && (
         <button type="button" onClick={onClick}>
           <div className="flex w-full flex-row items-center justify-between gap-16 rounded-[10px] bg-grey-50 p-24">
