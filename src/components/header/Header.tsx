822e49aeac3c65e5d35e106ef253150da9526f6c
Geon <geonulim@gmail.com>
2025-01-21 17:51:01 +0900

    feat: 로그인 여부에 따라 로그인 버튼 디자인 분리

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,29 @@
-'use client';
-
 import UserIcon from 'public/icons/user.svg';
 import LogoIcon from 'public/icons/logo.svg';
 import Link from 'next/link';
+import { getRefreshToken } from '@/utils/handleToken.util';
 
-const Header = () => {
+const Header = async () => {
+  const isLoggedIn = Boolean(await getRefreshToken());
+
   return (
     <header className="sticky top-0 z-50 flex h-48 w-full items-center justify-between bg-white px-16 py-12">
       <h1 className="sr-only">핸디버스</h1>
       <Link href="/">
         <LogoIcon fill="black" />
       </Link>
-      <Link href="/mypage">
-        <UserIcon />
-      </Link>
+      {isLoggedIn ? (
+        <Link href="/mypage">
+          <UserIcon />
+        </Link>
+      ) : (
+        <Link
+          href="/mypage"
+          className="flex h-full w-72 items-center justify-center rounded-[6px] bg-black text-14 font-400 text-white"
+        >
+          시작하기
+        </Link>
+      )}
     </header>
   );
 };
