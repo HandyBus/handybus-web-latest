5f03d1d38943b1b47f7a39b6faa54d93cd1615e5
jujeon <jwjsjwnsstjd@gmail.com>
2025-01-22 14:03:53 +0900

    fix: 홈의 서비스 준비중 모달에 primary variant 추가

--- src/app/(home)/@modal/page.tsx
+++ src/app/(home)/@modal/page.tsx
@@ -20,6 +20,7 @@ const Page = () => {
         description="더 나은 경험을 제공하기 위해 준비 중이에요.
 예약이 가능한 기존 사이트로 이동합니다."
         buttonLabels={{ back: '닫기', confirm: '이동하기' }}
+        variant="primary"
       />
     </>
   );
