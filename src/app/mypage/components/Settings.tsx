813bf4eeb4652e90265a25a27727d314d35bf5ae
jujeon <jwjsjwnsstjd@gmail.com>
2025-02-06 19:02:52 +0900

    feat: 마이페이지 의견남기기 추가

--- src/app/mypage/components/Settings.tsx
+++ src/app/mypage/components/Settings.tsx
@@ -17,6 +17,11 @@ const Settings = ({ couponCount, reviewCount }: Props) => {
         description={`${couponCount}장`}
       />
       <ListButton
+        title="의견 남기기"
+        href="https://docs.google.com/forms/d/1_PgL6I3XDhbAEae_CBMnwzaJXlTJdHswhUtriJ58fi8/edit" // TODO: 핸디버스 계정의 링크로 수정 필요
+        newTab={true}
+      />
+      <ListButton
         title="작성한 후기 조회"
         href="/mypage/reviews"
         description={`${reviewCount}건`}
