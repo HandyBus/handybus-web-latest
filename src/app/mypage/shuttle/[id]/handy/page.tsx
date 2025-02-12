883bdebc7b03be15aed6e6c5e2a4ec4fd35f0b24
Geon <geonulim@gmail.com>
2025-02-12 11:13:55 +0900

    fix: 핸디 url 제출 페이지 리다이렉트 수정

--- src/app/mypage/shuttle/[id]/handy/page.tsx
+++ src/app/mypage/shuttle/[id]/handy/page.tsx
@@ -3,6 +3,8 @@
 import AppBar from '@/components/app-bar/AppBar';
 import Button from '@/components/buttons/button/Button';
 import TextInput from '@/components/inputs/text-input/TextInput';
+import DeferredSuspense from '@/components/loading/DeferredSuspense';
+import Loading from '@/components/loading/Loading';
 import { CustomError } from '@/services/custom-error';
 import { usePutShuttleBus } from '@/services/shuttle-operation.service';
 import { useGetUserReservation } from '@/services/user-management.service';
@@ -54,7 +56,7 @@ interface Props {
 const Handy = ({ params }: Props) => {
   const { id } = params;
   const router = useRouter();
-  const { data } = useGetUserReservation(id);
+  const { data, isLoading } = useGetUserReservation(id);
   const { mutate: putShuttleBus } = usePutShuttleBus(id, {
     onSuccess: () => {
       toast.success('오픈채팅방 링크가 제출되었습니다.');
@@ -90,7 +92,7 @@ const Handy = ({ params }: Props) => {
   };
 
   useEffect(() => {
-    if (data?.reservation.handyStatus !== 'ACCEPTED') {
+    if (data && data.reservation.handyStatus !== 'ACCEPTED') {
       router.replace(`/mypage/shuttle/${id}`);
     }
   }, [data?.reservation.handyStatus]);
@@ -104,56 +106,62 @@ const Handy = ({ params }: Props) => {
   return (
     <>
       <AppBar>핸디 가이드</AppBar>
-      <main className="px-16">
-        <section className="pt-[60px]">
-          <h2 className="pb-12 text-28 font-700">‘핸디’ 가이드를 전달드려요</h2>
-          <p className="pb-12 text-16 font-400 text-grey-700">
-            이번 셔틀 운행 간 <span className="font-600">‘핸디’</span>로
-            지원해주셔서 감사드리며, 선정을 축하합니다!
-            <br />
-            <br />
-            핸디버스의 ‘핸디’는 안전한 셔틀 운행을 위한{' '}
-            <span className="font-600">현장 도우미</span>예요.
-            <br />
-            <br />
-            자세한 핸디 가이드는 아래 링크를 확인해주세요!
-          </p>
-          <Button
-            onClick={openHandyGuide}
-            className="mx-auto mt-[60px] block w-fit rounded-full bg-grey-100 px-16 text-grey-600-sub"
-          >
-            핸디 가이드 보러가기
-          </Button>
-        </section>
-        <section className="py-[60px]">
-          <h2 className="pb-12 text-28 font-700">오픈채팅방 링크 입력</h2>
-          <p className="pb-24 text-16 font-400 text-grey-700">
-            {isBusAssigned
-              ? TEXT.busAssigned.description
-              : TEXT.busNotAssigned.description}
-          </p>
-          <form
-            className="flex flex-col gap-12"
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <TextInput
-              name="openChatLink"
-              control={control}
-              setValue={setValue}
-              placeholder={
-                isBusAssigned
-                  ? TEXT.busAssigned.placeholder
-                  : TEXT.busNotAssigned.placeholder
-              }
-            >
-              링크 입력
-            </TextInput>
-            <Button type="submit" disabled={!isBusAssigned}>
-              링크 제출하기
-            </Button>
-          </form>
-        </section>
-      </main>
+      <DeferredSuspense isLoading={isLoading} fallback={<Loading />}>
+        {data && (
+          <main className="px-16">
+            <section className="pt-[60px]">
+              <h2 className="pb-12 text-28 font-700">
+                ‘핸디’ 가이드를 전달드려요
+              </h2>
+              <p className="pb-12 text-16 font-400 text-grey-700">
+                이번 셔틀 운행 간 <span className="font-600">‘핸디’</span>로
+                지원해주셔서 감사드리며, 선정을 축하합니다!
+                <br />
+                <br />
+                핸디버스의 ‘핸디’는 안전한 셔틀 운행을 위한{' '}
+                <span className="font-600">현장 도우미</span>예요.
+                <br />
+                <br />
+                자세한 핸디 가이드는 아래 링크를 확인해주세요!
+              </p>
+              <Button
+                onClick={openHandyGuide}
+                className="mx-auto mt-[60px] block w-fit rounded-full bg-grey-100 px-16 text-grey-600-sub"
+              >
+                핸디 가이드 보러가기
+              </Button>
+            </section>
+            <section className="py-[60px]">
+              <h2 className="pb-12 text-28 font-700">오픈채팅방 링크 입력</h2>
+              <p className="pb-24 text-16 font-400 text-grey-700">
+                {isBusAssigned
+                  ? TEXT.busAssigned.description
+                  : TEXT.busNotAssigned.description}
+              </p>
+              <form
+                className="flex flex-col gap-12"
+                onSubmit={handleSubmit(onSubmit)}
+              >
+                <TextInput
+                  name="openChatLink"
+                  control={control}
+                  setValue={setValue}
+                  placeholder={
+                    isBusAssigned
+                      ? TEXT.busAssigned.placeholder
+                      : TEXT.busNotAssigned.placeholder
+                  }
+                >
+                  링크 입력
+                </TextInput>
+                <Button type="submit" disabled={!isBusAssigned}>
+                  링크 제출하기
+                </Button>
+              </form>
+            </section>
+          </main>
+        )}
+      </DeferredSuspense>
     </>
   );
 };
