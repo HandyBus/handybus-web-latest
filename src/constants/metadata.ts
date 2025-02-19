a5471cffde93aaada9e200db798bbfefaae1dc10
Geon <geonulim@gmail.com>
2025-02-19 12:57:49 +0900

    fix: keyword 메타데이터 수정

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -1,14 +1,22 @@
 export const TITLE = '핸디버스';
 export const DESCRIPTION =
   '내 최애 콘서트로 가는 셔틀을 핸디버스에서 찾아보세요!';
+export const KEYWORDS =
+  '핸디버스, 수요응답형 모빌리티, 콘서트 셔틀, 공연 셔틀, 행사 셔틀, 맞춤형 셔틀, 콘서트 이동, 공연 이동, 행사 이동, 셔틀, 버스, 차대절, 버스 대절';
 export const URL = 'https://www.handybus.co.kr';
 export const OG_IMAGE_URL = URL + '/images/og-image.png';
 
-export const createMetadataWithOG = (
-  title: string,
-  imageUrl?: string,
-  location?: string,
-) => {
+export const createMetadataWithOG = ({
+  title,
+  keywords,
+  imageUrl,
+  location,
+}: {
+  title: string;
+  keywords?: string;
+  imageUrl?: string;
+  location?: string;
+}) => {
   const description = location
     ? `핸디버스를 통해 ${location}까지 내가 원하는 셔틀을 이용해보세요!`
     : DESCRIPTION;
@@ -16,6 +24,7 @@ export const createMetadataWithOG = (
   return {
     title,
     description,
+    keywords: KEYWORDS + ', ' + keywords,
     openGraph: {
       title,
       description,
