685632a34a0cad684b3f28cc32932da47628443a
Geon <geonulim@gmail.com>
2025-02-28 14:03:50 +0900

    wip

--- src/app/help/what-is-handy/page.tsx
+++ src/app/help/what-is-handy/page.tsx
@@ -1,8 +1,13 @@
-import Article from '@/components/article/Article';
-import { PropsWithChildren } from 'react';
-import ToastyCheck from 'public/icons/toasty-check.svg';
 import { Metadata } from 'next';
 import Header from '@/components/header/Header';
+import Image from 'next/image';
+import MainImage from './images/main.png';
+import DescriptionImage1 from './images/description1.png';
+import DescriptionImage2 from './images/description2.png';
+import DescriptionImage3 from './images/description3.png';
+import DescriptionImage4 from './images/description4.png';
+import CouponImage from './images/coupon.png';
+import BottomImage from './images/bottom.png';
 
 export const metadata: Metadata = {
   title: '핸디란?',
@@ -12,57 +17,60 @@ const AboutPage = () => {
   return (
     <>
       <Header />
-      <main className="w-full flex-grow pb-12">
-        <Article titleSize="large" richTitle="핸디버스의 ‘핸디’란?">
-          <p className="w-full flex-col gap-8 px-16 pt-12 text-16 font-500 text-grey-700">
-            핸디버스의 ‘핸디’는 안전한 셔틀 운행을 위한 현장 도우미예요. 셔틀 당
-            선착순으로 한 분을 뽑아요.
+      <main className="flex w-full grow flex-col gap-40 pb-12">
+        <section className="flex h-[366px] w-full flex-col items-center justify-end bg-[#E6FFF7]">
+          <p className="pb-4 pt-40 text-16 font-600">
+            승차권 <span className="text-primary-main">50%</span> 할인
           </p>
-        </Article>
-        <Article titleSize="small" richTitle="딱 이 세가지가 필요해요">
-          <ul className="flex w-full flex-col gap-8 px-16 pt-12">
-            <Desc>만 19세 이상 성인</Desc>
-            <Desc>왕복 탑승 이용 고객</Desc>
-            <Desc>전화번호 공개 가능</Desc>
-          </ul>
-        </Article>
-        <Article titleSize="small" richTitle="딱 이 세가지만 하면 돼요">
-          <ul className="flex w-full flex-col gap-8 px-16 pt-12">
-            <Desc checked>다 왔는지 탑승 인원 체크</Desc>
-            <Desc checked>기사님과 간단한 소통</Desc>
-            <Desc checked>핸디버스에게 출발, 도착 알림</Desc>
-          </ul>
-        </Article>
-        <Article titleSize="small" richTitle="이런 혜택이 있어요">
-          <p className="mb-28 w-full flex-col gap-8 px-16 pt-12 text-16 font-500 text-grey-700">
-            다음에 또 핸디버스를 이용하실 때{' '}
-            <strong className="font-600 text-primary-main">
-              이용료의 절반을 할인
-            </strong>{' '}
-            받을 수 있는 쿠폰을 드려요!
+          <h1 className="pb-36 text-center text-28 font-700 leading-[39px]">
+            핸디로 지원하고
+            <br />
+            <span className="text-primary-main">최대 4만원</span> 받으세요
+          </h1>
+          <div className="relative h-188 w-full">
+            <Image
+              src={MainImage}
+              alt="핸디 소개 이미지"
+              fill
+              className="object-contain"
+            />
+          </div>
+        </section>
+        <section className="flex flex-col gap-8 px-16">
+          <h2 className="text-22 font-700">핸디가 뭐에요?</h2>
+          <p className="text-16 font-400 text-grey-700">
+            함께 가는 팬들을 연결하는{' '}
+            <span className="inline-block rounded-[] bg-[#E6FFF7] px-12 text-16 font-600 text-primary-main">
+              커넥터
+            </span>
+            예요. 셔틀 내에서 행사장까지의 여정을 원활하게 만들고, 팬들과의
+            소통을 통해 즐거운 분위기를 만들어주는 중요한 역할을 해요. 즐거운
+            분위기를 만들어주는 중요한 역할을 해요.
           </p>
-        </Article>
+          <p className="text-14 font-400 text-grey-400">
+            * 핸디는 지원자 중 한 분만 선정돼요.
+          </p>
+        </section>
+        <section className="flex flex-col gap-8 px-16">
+          <h2 className="text-22 font-700">핸디가 뭐에요?</h2>
+          <p className="text-16 font-400 text-grey-700">
+            너무 어려울 것 같다고요? 전혀 그렇지 않아요.
+            <br />
+            누구든 할 수 있어요! 핸디가 하는 일을 알려드릴게요.
+          </p>
+          <article>
+            <h3 className="mb-12 flex h-24 items-center gap-8 text-18 font-600">
+              <span className="rounded-[6px] bg-[#F3F3F3] px-8 text-grey-700">
+                1
+              </span>
+              출발/도착 알림
+            </h3>
+            <p>오픈챗을 통해 셔틀버스의 출발과 도착 알림을 보내요.</p>
+          </article>
+        </section>
       </main>
     </>
   );
 };
 
 export default AboutPage;
-
-const Desc = ({
-  checked,
-  children,
-}: PropsWithChildren<{ checked?: boolean }>) => {
-  return (
-    <li className="flex size-16 h-fit w-full flex-row items-center gap-[10px] rounded-[8px] bg-grey-50 px-12 py-16 text-16 font-500">
-      <figure
-        className={
-          checked ? 'text-20 text-primary-main' : 'text-20 text-grey-600-sub'
-        }
-      >
-        <ToastyCheck />
-      </figure>
-      <p className="text-16 font-500 text-grey-700">{children}</p>
-    </li>
-  );
-};
