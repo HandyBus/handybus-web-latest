ddfa14c4e3e025477462d64a1637c09be3ff9450
Geon <geonulim@gmail.com>
2025-03-10 17:28:42 +0900

    feat: 최근 로그인 표시

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,8 +6,9 @@ import Naver from 'public/icons/naver.svg';
 import Link from 'next/link';
 import { OAUTH } from '@/constants/oauth';
 import usePreventScroll from '@/hooks/usePreventScroll';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
+import { getLastLogin, setLastLogin } from '@/utils/localStorage';
 
 const Login = () => {
   usePreventScroll();
@@ -27,6 +28,17 @@ const Login = () => {
     handleRedirectUrl();
   }, [searchParams]);
 
+  const [lastLoginState, setLastLoginState] = useState<
+    'kakao' | 'naver' | null
+  >(null);
+
+  useEffect(() => {
+    const newLastLogin = getLastLogin();
+    if (newLastLogin) {
+      setLastLoginState(newLastLogin);
+    }
+  }, []);
+
   return (
     <main className="flex grow flex-col">
       <section className="-my-8 flex flex-1 grow flex-col items-center justify-center gap-16">
@@ -42,22 +54,36 @@ const Login = () => {
           지금 가입하면 <span className="text-primary-main">1만원</span> 쿠폰
           즉시 제공!
         </p>
-        <Link
-          href={OAUTH.kakao()}
-          replace
-          className="mb-12 flex h-52 items-center justify-center gap-8 rounded-[8px] bg-[#FEE500] text-16 font-600 text-black/85"
+        <button
+          onClick={() => setLastLogin('kakao')}
+          type="button"
+          className="w-full rounded-[8px]"
         >
-          <Kakao />
-          카카오로 시작하기
-        </Link>
-        <Link
-          href={OAUTH.naver()}
-          replace
-          className="flex h-52 items-center justify-center gap-8 rounded-[8px] bg-[#03C75A] text-16 font-600 text-white"
+          <Link
+            href={OAUTH.kakao()}
+            replace
+            className="relative mb-12 flex h-52 items-center justify-center gap-8 rounded-[8px] bg-[#FEE500] text-16 font-600 text-black/85"
+          >
+            <Kakao />
+            카카오로 시작하기
+            {lastLoginState === 'kakao' && <LastLoginChip />}
+          </Link>
+        </button>
+        <button
+          onClick={() => setLastLogin('naver')}
+          type="button"
+          className="w-full rounded-[8px]"
         >
-          <Naver />
-          네이버로 시작하기
-        </Link>
+          <Link
+            href={OAUTH.naver()}
+            replace
+            className="relative flex h-52 items-center justify-center gap-8 rounded-[8px] bg-[#03C75A] text-16 font-600 text-white"
+          >
+            <Naver />
+            네이버로 시작하기
+            {lastLoginState === 'naver' && <LastLoginChip />}
+          </Link>
+        </button>
       </section>
       <section className="flex flex-1 grow flex-col">
         <p className="mx-16 mt-40 border-t border-[#F3F3F3] pt-16 text-center text-12 font-500 text-grey-400">
@@ -79,3 +105,11 @@ const Login = () => {
 };
 
 export default Login;
+
+const LastLoginChip = () => {
+  return (
+    <div className="absolute -top-[5px] right-8 flex h-[26px] items-center justify-center rounded-full bg-[#181F29] px-8 text-10 font-600 text-white">
+      최근 로그인
+    </div>
+  );
+};
--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,29 +1,35 @@
 // 예약 완료
 export const IS_RESERVATION_COMPLETED = 'is-reservation-completed';
-
 export const setReservationCompleted = () => {
   localStorage.setItem(IS_RESERVATION_COMPLETED, '1');
 };
-
 export const getReservationCompleted = () => {
   return Boolean(localStorage.getItem(IS_RESERVATION_COMPLETED));
 };
-
 export const removeReservationCompleted = () => {
   localStorage.removeItem(IS_RESERVATION_COMPLETED);
 };
 
 // 첫 가입
 export const IS_FIRST_SIGNUP = 'is-first-signup';
-
 export const setFirstSignup = () => {
   localStorage.setItem(IS_FIRST_SIGNUP, '1');
 };
-
 export const getFirstSignup = () => {
   return Boolean(localStorage.getItem(IS_FIRST_SIGNUP));
 };
-
 export const removeFirstSignup = () => {
   localStorage.removeItem(IS_FIRST_SIGNUP);
+};
+
+// 최근 로그인
+export const LAST_LOGIN = 'last-login';
+export const setLastLogin = (type: 'kakao' | 'naver') => {
+  localStorage.setItem(LAST_LOGIN, type);
+};
+export const getLastLogin = () => {
+  return localStorage.getItem(LAST_LOGIN) as 'kakao' | 'naver' | null;
+};
+export const removeLastLogin = () => {
+  localStorage.removeItem(LAST_LOGIN);
 };
