d04f38cf7e48eac54697b778e2c99a1bece390cb
jujeon <jwjsjwnsstjd@gmail.com>
2025-03-13 14:33:25 +0900

    refactor: Article components props

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -5,13 +5,13 @@ import Link from 'next/link';
 interface Props extends HTMLProps<HTMLDivElement> {
   richTitle: ReactNode;
   showMore?: string | UrlObject;
-  titleSize?: 'small' | 'large';
+  titleSize?: string;
 }
 
 const Article = ({
   children,
   richTitle,
-  titleSize = 'small',
+  titleSize = 'text-22',
   showMore,
   ...props
 }: Props) => {
@@ -29,18 +29,14 @@ import ChevronRightEm from 'public/icons/chevron-right-em.svg';
 
 interface TitleProps {
   title: ReactNode;
-  titleSize: 'small' | 'large';
+  titleSize: string;
   showMore?: string | UrlObject;
 }
 
 const Title = ({ title, titleSize, showMore }: TitleProps) => {
   return (
     <header className="flex w-full flex-row justify-between px-16 pb-4 pt-60">
-      <h2
-        className={`${titleSize === 'small' ? 'text-22' : 'text-28'} font-700`}
-      >
-        {title}
-      </h2>
+      <h2 className={`${titleSize} font-700`}>{title}</h2>
       {showMore && (
         <Link href={showMore}>
           <span className="inline-flex cursor-pointer items-center gap-[2px] break-keep text-12 font-400 text-grey-600-sub">
