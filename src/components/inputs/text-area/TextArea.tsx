b39d0b3d3c0dac9645c3ff48cd3be4d623393bd5
Geon <geonulim@gmail.com>
2025-03-19 10:59:41 +0900

    design: text area v2 디자인

--- src/components/inputs/text-area/TextArea.tsx
+++ src/components/inputs/text-area/TextArea.tsx
@@ -3,6 +3,7 @@ import {
   UseControllerProps,
   useController,
 } from 'react-hook-form';
+import { customTwMerge } from 'tailwind.config';
 
 interface Props<T extends FieldValues> extends UseControllerProps<T> {
   placeholder?: string;
@@ -21,10 +22,15 @@ const TextArea = <T extends FieldValues>({
       <textarea
         {...field}
         placeholder={placeholder}
-        className={`h-[136px] w-full resize-none rounded-xl border border-brand-grey-100 p-12 text-16 font-400 outline-none placeholder:text-brand-grey-300 ${fieldState?.error ? 'border-basic-red-500' : 'focus:border-brand-primary-100'}`}
+        className={customTwMerge(
+          'h-160 rounded-[12px] border border-brand-grey-200 p-12 text-16 font-500 outline-none placeholder:text-brand-grey-400 disabled:text-brand-grey-300',
+          fieldState?.error
+            ? 'border-basic-red-500'
+            : 'focus:border-brand-primary-100',
+        )}
       />
       {fieldState?.error?.message && (
-        <div className="h-[20px] pl-12 text-12 font-400 text-basic-red-100">
+        <div className="h-[20px] pl-12 text-12 font-400 text-basic-red-400">
           {fieldState?.error?.message}
         </div>
       )}
