6188930c9abecfdddc2b98079af8041fc23d8cb9
jujeon <jwjsjwnsstjd@gmail.com>
2025-04-08 11:09:23 +0900

    feat: 솔직한 이용후기 마크업

--- src/app/(home)/@bot/components/PromotionReviews.tsx
+++ src/app/(home)/@bot/components/PromotionReviews.tsx
@@ -4,7 +4,7 @@ import { STATIC_REVIEWS } from '@/app/help/reviews/review';
 import { DEFAULT_SSG_REVALIDATE_TIME } from '@/constants/common';
 import ArticleV2 from '@/components/article/ArticleV2';
 import { ReviewsViewEntity } from '@/types/review.type';
-import Image from 'next/image';
+import UserProfile from '@/components/header/UserProfile';
 
 export const revalidate = DEFAULT_SSG_REVALIDATE_TIME;
 
@@ -22,40 +22,30 @@ const PromotionReview = async () => {
   return (
     <ArticleV2 richTitle="솔직한 이용 후기" showMore="/help/reviews">
       {top3.map((review: ReviewsViewEntity) => (
-        <div key={review.reviewId} className="flex flex-col gap-[10px] p-8">
-          <div>
+        <div key={review.reviewId} className="flex flex-col p-8">
+          <div className="flex flex-col gap-[2px]">
             <div className="flex items-center justify-between">
               <div className="flex items-center gap-[6px]">
-                {review.userProfileImage ? (
-                  <Image
-                    src={review.userProfileImage}
-                    alt={review.userNickname}
-                    fill
-                    className="rounded-full"
-                  />
-                ) : (
-                  <div className="h-24 w-24 rounded-full bg-basic-grey-50">
-                    {review.userNickname.slice(0, 1)}
-                  </div>
-                )}
+                <UserProfile
+                  nickname={review.userNickname}
+                  profileImage={review.userProfileImage}
+                />
                 <p className="text-12 font-500 leading-[160%] text-basic-black">
                   {review.userNickname}
                 </p>
               </div>
               <Rating size="medium" value={review.rating} />
             </div>
+            <FeedbackGroup />
+            <p className="line-clamp-2 overflow-hidden text-14 font-500 leading-[160%] text-basic-grey-600">
+              {review.content}
+            </p>
+            <p className="text-12 font-500 leading-[160%] text-basic-grey-400">
+              {review.eventName}
+            </p>
           </div>
-          <p>예약과정 매우 만족스러워요 | 탑승 매우 만족스러워요</p>
-          <p className="line-clamp-2 overflow-hidden text-14 font-500 leading-[160%] text-basic-grey-600">
-            {review.content}
-          </p>
-          <p className="text-12 font-500 leading-[160%] text-basic-black">
-            {review.eventName}
-          </p>
-          <div className="flex w-full items-center justify-end gap-4">
-            <div className="rounded-[42px] bg-basic-grey-50 px-8 py-4 text-10 font-600 leading-[160%] text-basic-grey-700">
-              이달의 후기
-            </div>
+          <div className="mt-4 flex w-full items-center justify-end gap-4">
+            <MonthlyReviewChip />
             <p className="text-12 font-500 leading-[160%] text-basic-grey-500">
               {review.createdAt}
             </p>
@@ -67,3 +57,48 @@ const PromotionReview = async () => {
 };
 
 export default PromotionReview;
+
+const MonthlyReviewChip = () => {
+  return (
+    <div className="rounded-[42px] bg-basic-grey-50 px-8 py-4 text-10 font-600 leading-[160%] text-basic-grey-700">
+      이달의 후기
+    </div>
+  );
+};
+
+const FeedbackGroup = () => {
+  return (
+    <div className="flex gap-[6px] ">
+      <FeedbackCard type="예약과정" text="매우 만족스러워요" />
+      <p className="text-12 font-500 leading-[160%] text-basic-grey-200">|</p>
+      <FeedbackCard type="탑승" text="매우 만족스러워요" />
+    </div>
+  );
+};
+
+const FeedbackCard = ({
+  type,
+  text,
+}: {
+  type: FeedbackType;
+  text: FeedbackText;
+}) => {
+  return (
+    <div className="gap-2 flex items-center gap-4">
+      <p className="text-12 font-500 leading-[160%] text-basic-grey-700">
+        {type}
+      </p>
+      <p className="text-12 font-500 leading-[160%] text-basic-grey-500">
+        {text}
+      </p>
+    </div>
+  );
+};
+
+type FeedbackType = '예약과정' | '탑승';
+type FeedbackText =
+  | '매우 불만족스러워요'
+  | '불만족스러워요'
+  | '보통스러워요'
+  | '만족스러워요'
+  | '매우 만족스러워요';
