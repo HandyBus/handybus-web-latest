9c5490c4142ea072c9cec2e840f5c712e3867108
jujeon <jwjsjwnsstjd@gmail.com>
2025-04-17 17:41:21 +0900

    refactor: 불필요한 상수 제거

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,18 +2,6 @@ import Image from 'next/image';
 import Badge from '../badge/Badge';
 import Link from 'next/link';
 
-const CARD_SIZE = {
-  LARGE: 'w-[232px] h-[309px]',
-  MEDIUM: 'w-[145px] h-[193px]',
-  SMALL: 'w-[100px] h-[133px]',
-} as const;
-
-const CARD_ROUNDED = {
-  LARGE: 'rounded-[14px]',
-  MEDIUM: 'rounded-8',
-  SMALL: 'rounded-8',
-} as const;
-
 const DEMAND_ONGOING_BADGE_CLASS_NAME =
   'inline-flex shrink-0 bg-basic-blue-100 leading-[160%] text-basic-blue-400';
 
@@ -84,7 +72,6 @@ const Card = ({
 export default Card;
 
 const LargeCard = ({
-  variant,
   image,
   order = 1,
   isSaleStarted,
@@ -95,48 +82,43 @@ const LargeCard = ({
   return (
     <Link
       href={href || ''}
-      className={`block ${CARD_SIZE[variant]} relative shrink-0 border-[1px] border-[#181F29] border-opacity-[0.08] ${CARD_ROUNDED[variant]}`}
+      className={`relative block h-[309px] w-[232px] shrink-0 rounded-[14px] border-[1px] border-[#181F29] border-opacity-[0.08]`}
     >
       <Image
         src={image || '/images/default-event.png'}
         alt="card"
         fill
-        className={`object-cover ${CARD_ROUNDED[variant]}`}
+        className={`rounded-[14px] object-cover`}
       />
-      {variant === 'LARGE' && (
-        <>
-          {order && (
-            <div className="absolute left-0 right-0 flex h-[50px] w-[43px] items-center  justify-center rounded-br-12 rounded-tl-12 bg-[#00C896CC] text-20 font-700 leading-[140%] text-basic-white">
-              {order}
-            </div>
-          )}
-          <div className="absolute bottom-0 left-0 right-0 h-108 w-full break-words rounded-b-12  bg-[#181F29] bg-opacity-60 p-16 ">
-            <p className="line-clamp-2 break-all text-18 font-600 leading-[140%] text-basic-white">
-              {title}
-            </p>
-            <div className="mt-[2px] flex items-center gap-4">
-              <p
-                className={`text-16 font-600 leading-[140%] ${
-                  isSaleStarted ? 'text-basic-white' : 'text-basic-grey-300'
-                }`}
-              >
-                {isSaleStarted ? price : '판매대기'}
-              </p>
-              {!isSaleStarted && (
-                <Badge className={`${DEMAND_ONGOING_BADGE_CLASS_NAME}`}>
-                  수요조사 진행 중
-                </Badge>
-              )}
-            </div>
-          </div>
-        </>
+      {order && (
+        <div className="absolute left-0 right-0 flex h-[50px] w-[43px] items-center  justify-center rounded-br-12 rounded-tl-12 bg-[#00C896CC] text-20 font-700 leading-[140%] text-basic-white">
+          {order}
+        </div>
       )}
+      <div className="absolute bottom-0 left-0 right-0 h-108 w-full break-words rounded-b-12  bg-[#181F29] bg-opacity-60 p-16 ">
+        <p className="line-clamp-2 break-all text-18 font-600 leading-[140%] text-basic-white">
+          {title}
+        </p>
+        <div className="mt-[2px] flex items-center gap-4">
+          <p
+            className={`text-16 font-600 leading-[140%] ${
+              isSaleStarted ? 'text-basic-white' : 'text-basic-grey-300'
+            }`}
+          >
+            {isSaleStarted ? price : '판매대기'}
+          </p>
+          {!isSaleStarted && (
+            <Badge className={`${DEMAND_ONGOING_BADGE_CLASS_NAME}`}>
+              수요조사 진행 중
+            </Badge>
+          )}
+        </div>
+      </div>
     </Link>
   );
 };
 
 const MediumCard = ({
-  variant,
   image,
   isSaleStarted,
   title = 'ATEEZ 2024 FANMEETING 〈ATINY’S VOYAGE FROM A TO Z〉',
@@ -147,39 +129,40 @@ const MediumCard = ({
   return (
     <Link href={href || ''} className="block w-[145px]">
       <div
-        className={`${CARD_SIZE[variant]} relative shrink-0 border-[1px] border-[#181F29] border-opacity-[0.08] ${CARD_ROUNDED[variant]}`}
+        className={`relative h-[193px] w-[145px] shrink-0 rounded-8 border-[1px] border-[#181F29] border-opacity-[0.08]`}
       >
         <Image
           src={image || '/images/default-event.png'}
           alt="card"
           fill
-          className={`object-cover ${CARD_ROUNDED[variant]}`}
+          className={`rounded-8 object-cover`}
         />
       </div>
-      <p className="line-clamp-2 break-all text-14 font-600 leading-[140%] text-basic-black">
-        {title}
-      </p>
-      <p className="text-12 font-500 leading-[160%] text-basic-black ">
-        {date}
-      </p>
-      <p
-        className={`text-14 font-600 leading-[140%] ${
-          isSaleStarted ? 'text-basic-black' : 'text-basic-grey-500'
-        }`}
-      >
-        {isSaleStarted ? price : '판매대기'}
-      </p>
-      {!isSaleStarted && (
-        <Badge className={`mt-4 ${DEMAND_ONGOING_BADGE_CLASS_NAME}`}>
-          수요조사 진행 중
-        </Badge>
-      )}
+      <div className="py-12 pl-4 pr-12">
+        <p className="line-clamp-2 break-all text-14 font-600 leading-[140%] text-basic-black">
+          {title}
+        </p>
+        <p className="text-12 font-500 leading-[160%] text-basic-black ">
+          {date}
+        </p>
+        <p
+          className={`text-14 font-600 leading-[140%] ${
+            isSaleStarted ? 'text-basic-black' : 'text-basic-grey-500'
+          }`}
+        >
+          {isSaleStarted ? price : '판매대기'}
+        </p>
+        {!isSaleStarted && (
+          <Badge className={`mt-4 ${DEMAND_ONGOING_BADGE_CLASS_NAME}`}>
+            수요조사 진행 중
+          </Badge>
+        )}
+      </div>
     </Link>
   );
 };
 
 const SmallCard = ({
-  variant,
   image,
   isSaleStarted,
   title = 'ATEEZ 2024 FANMEETING 〈ATINY’S VOYAGE FROM A TO Z〉',
@@ -191,13 +174,13 @@ const SmallCard = ({
   return (
     <Link href={href || ''} className="flex gap-12">
       <div
-        className={`${CARD_SIZE[variant]} relative shrink-0 border-[1px] border-[#181F29] border-opacity-[0.08] ${CARD_ROUNDED[variant]}`}
+        className={`relative h-[133px] w-[100px] shrink-0 rounded-8 border-[1px] border-[#181F29] border-opacity-[0.08]`}
       >
         <Image
           src={image || '/images/default-event.png'}
           alt="card"
           fill
-          className={`object-cover ${CARD_ROUNDED[variant]}`}
+          className={`rounded-8 object-cover`}
         />
       </div>
       <div>
