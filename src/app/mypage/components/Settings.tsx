27c3a9b1b67bda3cd8ef6fe0d0aaa8d97dce35c6
Geon <geonulim@gmail.com>
2025-04-30 18:04:26 +0900

    feat: 마이페이지 링크 연결

--- src/app/mypage/components/Settings.tsx
+++ src/app/mypage/components/Settings.tsx
@@ -7,25 +7,25 @@ const Settings = () => {
     <div className="flex flex-col gap-[18px] px-16">
       <section>
         <h3 className="pb-8 text-14 font-600 text-basic-grey-500">서비스</h3>
-        <ListButton href="/mypage/coupons">빈자리 알림</ListButton>
+        <ListButton href="/mypage/seat-alarm">빈자리 알림</ListButton>
       </section>
       <section>
         <h3 className="pb-8 text-14 font-600 text-basic-grey-500">
           핸디버스 완벽 정복
         </h3>
-        <ListButton href="/mypage/coupons">핸디버스 가이드</ListButton>
-        <ListButton href="/mypage/coupons">핸디 알아보기</ListButton>
-        <ListButton href="/mypage/coupons">서비스 소개</ListButton>
+        <ListButton href="/help/handybus-guide">핸디버스 가이드</ListButton>
+        <ListButton href="/help/what-is-handy">핸디 알아보기</ListButton>
+        <ListButton href="/help/about">서비스 소개</ListButton>
       </section>
       <div className="-mx-16 h-[8px] bg-basic-grey-50" />
       <section>
-        <ListButton href="/mypage/coupons" hideArrow>
+        <ListButton href="/help/faq" hideArrow>
           도움말
         </ListButton>
-        <ListButton href="/mypage/coupons" hideArrow>
+        <ListButton href="/mypage/notice-board" hideArrow>
           공지사항
         </ListButton>
-        <ListButton href="/mypage/coupons" hideArrow>
+        <ListButton href="/mypage/settings" hideArrow>
           환경설정
         </ListButton>
       </section>
