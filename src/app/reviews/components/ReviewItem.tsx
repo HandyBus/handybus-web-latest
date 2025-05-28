82bc34c70d844c81ce5d70310abcdf757a902540
jujeon <jwjsjwnsstjd@gmail.com>
2025-05-28 14:12:25 +0900

    feat: 리뷰 사진 확대 기능

--- src/app/reviews/components/ReviewItem.tsx
+++ src/app/reviews/components/ReviewItem.tsx
@@ -4,6 +4,7 @@ import { ReviewsViewEntity } from '@/types/review.type';
 import { useState } from 'react';
 import Image from 'next/image';
 import { dateString } from '@/utils/dateString.util';
+import ImageModal from './ImageModal';
 
 interface Props {
   review: ReviewsViewEntity;
@@ -11,68 +12,84 @@ interface Props {
 
 const ReviewItem = ({ review }: Props) => {
   const [isExpanded, setIsExpanded] = useState(false);
+  const [openImageUrl, setOpenImageUrl] = useState<string | null>(null);
 
   return (
-    <article>
-      <div key={review.reviewId} className={`flex flex-col rounded-8 p-12`}>
-        <div className="flex flex-col items-start gap-[2px]">
-          <div className="flex w-full items-center justify-between">
-            <div className="flex items-center gap-[6px]">
-              <UserProfile
-                nickname={review.userNickname}
-                profileImage={review.userProfileImage}
-              />
-              <p className="text-12 font-500 leading-[160%] text-basic-black">
-                {review.userNickname}
-              </p>
+    <>
+      <article>
+        <div key={review.reviewId} className={`flex flex-col rounded-8 p-12`}>
+          <div className="flex flex-col items-start gap-[2px]">
+            <div className="flex w-full items-center justify-between">
+              <div className="flex items-center gap-[6px]">
+                <UserProfile
+                  nickname={review.userNickname}
+                  profileImage={review.userProfileImage}
+                />
+                <p className="text-12 font-500 leading-[160%] text-basic-black">
+                  {review.userNickname}
+                </p>
+              </div>
+              <Rating size="medium" value={review.rating} />
             </div>
-            <Rating size="medium" value={review.rating} />
-          </div>
-          <FeedbackGroup />
-          <figure className="flex gap-4">
-            {review.reviewImages &&
-              review.reviewImages.length > 0 &&
-              review.reviewImages?.map((image, index) => {
-                return (
-                  <div className="relative h-60 w-60" key={index}>
-                    <Image
-                      src={image.imageUrl}
-                      alt={`${review.eventName} 후기 이미지 ${index + 1}`}
-                      fill
-                      className="rounded-8 object-cover"
-                    />
-                  </div>
-                );
-              })}
-          </figure>
-          <p
-            className={`line-clamp-2 overflow-hidden text-14 font-500 leading-[160%] text-basic-grey-600 ${
-              isExpanded ? 'line-clamp-none' : ''
-            }`}
-          >
-            {review.content}
-          </p>
-          {review.content.length > 100 && (
-            <button
-              className="text-14 font-500 leading-[160%] text-basic-grey-500"
-              onClick={() => setIsExpanded((prev) => !prev)}
+            <FeedbackGroup />
+            <figure className="flex gap-4">
+              {review.reviewImages &&
+                review.reviewImages.length > 0 &&
+                review.reviewImages?.map((image, index) => {
+                  return (
+                    <button
+                      type="button"
+                      className="relative h-60 w-60"
+                      key={index}
+                      onClick={() => {
+                        setOpenImageUrl(image.imageUrl);
+                      }}
+                    >
+                      <Image
+                        src={image.imageUrl}
+                        alt={`${review.eventName} 후기 이미지 ${index + 1}`}
+                        fill
+                        className="rounded-8 object-cover"
+                      />
+                    </button>
+                  );
+                })}
+            </figure>
+            <p
+              className={`line-clamp-2 overflow-hidden text-14 font-500 leading-[160%] text-basic-grey-600 ${
+                isExpanded ? 'line-clamp-none' : ''
+              }`}
             >
-              {isExpanded ? '접기' : '더보기'}
-            </button>
-          )}
-          <p className="text-12 font-500 leading-[160%] text-basic-grey-400">
-            {review.eventName}
-          </p>
+              {review.content}
+            </p>
+            {review.content.length > 100 && (
+              <button
+                className="text-14 font-500 leading-[160%] text-basic-grey-500"
+                onClick={() => setIsExpanded((prev) => !prev)}
+              >
+                {isExpanded ? '접기' : '더보기'}
+              </button>
+            )}
+            <p className="text-12 font-500 leading-[160%] text-basic-grey-400">
+              {review.eventName}
+            </p>
+          </div>
+          <div className="mt-4 flex w-full items-center justify-end gap-4">
+            <p className="text-12 font-500 leading-[160%] text-basic-grey-500">
+              {dateString(review.createdAt, {
+                showWeekday: false,
+              })}
+            </p>
+          </div>
         </div>
-        <div className="mt-4 flex w-full items-center justify-end gap-4">
-          <p className="text-12 font-500 leading-[160%] text-basic-grey-500">
-            {dateString(review.createdAt, {
-              showWeekday: false,
-            })}
-          </p>
-        </div>
-      </div>
-    </article>
+      </article>
+      {openImageUrl && (
+        <ImageModal
+          imageUrl={openImageUrl}
+          onClose={() => setOpenImageUrl(null)}
+        />
+      )}
+    </>
   );
 };
 
