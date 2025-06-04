0d74c0e75c5192d31f56ad85991370da46f5d8da
Geon <geonulim@gmail.com>
2025-06-04 18:46:59 +0900

    feat: 예약하기 FQA 업데이트

--- src/data/faq/index.tsx
+++ src/data/faq/index.tsx
@@ -9,6 +9,8 @@ import FAQReservation6 from './reservation-6-sold-out-additional-shuttle.mdx';
 import FAQReservation7 from './reservation-7-check-reservation-details.mdx';
 import FAQReservation8 from './reservation-8-change-boarding-location.mdx';
 import FAQReservation9 from './reservation-9-cancel-reservation.mdx';
+import FAQReservation10 from './reservation-10.mdx';
+import FAQReservation11 from './reservation-11.mdx';
 import FAQBoarding1 from './boarding-1-does-shuttle-stop-at-rest-area.mdx';
 import FAQBoarding2 from './boarding-2-what-bus-is-used-for-service.mdx';
 import FAQBoarding3 from './boarding-3-how-are-seats-assigned.mdx';
@@ -32,8 +34,12 @@ export const faqs: FAQ[] = (
       'reservation',
       '수요조사로 몇 명이 모여야 셔틀이 열리나요?',
     ],
-    [FAQReservation4, 'reservation', '수요조사 중 원하는 정류장이 없어요.'],
-    [FAQReservation5, 'reservation', '셔틀 운행은 언제 확정되나요?'],
+    [FAQReservation4, 'reservation', '셔틀 운행은 언제 확정되나요?'],
+    [
+      FAQReservation5,
+      'reservation',
+      '열린 셔틀 중 예약하고 싶은 정류장이 없어요. ',
+    ],
     [
       FAQReservation6,
       'reservation',
@@ -42,6 +48,16 @@ export const faqs: FAQ[] = (
     [FAQReservation7, 'reservation', '예약 내역을 확인하고 싶어요.'],
     [FAQReservation8, 'reservation', '탑승지를 변경하고 싶어요.'],
     [FAQReservation9, 'reservation', '예약을 취소하고 싶어요'],
+    [
+      FAQReservation10,
+      'reservation',
+      '탑승자와 신청자가 다른 경우 어떡하나요?',
+    ],
+    [
+      FAQReservation11,
+      'reservation',
+      '콘서트가 예정보다 늦게 끝나면 어떡하나요?',
+    ],
     [FAQBoarding1, 'boarding', '운행 시 휴게소에 들르나요?'],
     [FAQBoarding2, 'boarding', '어떤 버스로 운행되나요?'],
     [FAQBoarding3, 'boarding', '좌석은 어떻게 정해지나요?'],
