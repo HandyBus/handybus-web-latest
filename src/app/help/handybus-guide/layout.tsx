37a64f48203688964a442863a7e5188eeda9a5ef
jujeon <jwjsjwnsstjd@gmail.com>
2025-06-04 13:15:10 +0900

    feat: 핸디버스 가이드 핸디팟 소개 추가

---
+++ src/app/help/handybus-guide/layout.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '핸디버스 가이드',
+  description:
+    '핸디버스 수요조사 참여 그리고 셔틀 예약부터 탑승까지의 상세 가이드를 제공합니다.',
+};
+
+const HandybusGuideLayout = ({ children }: { children: React.ReactNode }) => {
+  return <>{children}</>;
+};
+
+export default HandybusGuideLayout;
--- src/app/help/handybus-guide/page.tsx
+++ src/app/help/handybus-guide/page.tsx
@@ -1,5 +1,6 @@
+'use client';
+
 import Button from '@/components/buttons/button/Button';
-import { Metadata } from 'next';
 import Image, { StaticImageData } from 'next/image';
 import guideImage from './images/handybus-guide.png';
 import guideImageStep1 from './images/handybus-guide-step-1.png';
@@ -8,24 +9,35 @@ import guideImageStep3 from './images/handybus-guide-step-3.png';
 import guideImageStep4 from './images/handybus-guide-step-4.png';
 import guideImageStep5 from './images/handybus-guide-step-5.png';
 import guideImageStep6 from './images/handybus-guide-step-6.png';
+import handypartyGuideImageStep1 from './images/handyparty-guide-step-1.png';
+import handypartyGuideImageStep2 from './images/handyparty-guide-step-2.png';
+import handypartyGuideImageStep3 from './images/handyparty-guide-step-3.png';
+import handypartyGuideImageStep4 from './images/handyparty-guide-step-4.png';
+import handypartyGuideImageStep5 from './images/handyparty-guide-step-5.png';
+import handypartyGuideImageStep6 from './images/handyparty-guide-step-6.png';
+import handypartyGuideImageStep7 from './images/handyparty-guide-step-7.png';
 import Link from 'next/link';
 import ChevronRightEmIcon from 'public/icons/chevron-right-em.svg';
 import Header from '@/components/header/Header';
-
-export const metadata: Metadata = {
-  title: '핸디버스 가이드',
-  description:
-    '핸디버스 수요조사 참여 그리고 셔틀 예약부터 탑승까지의 상세 가이드를 제공합니다.',
-};
+import Tabs from '@/components/tab/Tabs';
+import { ReactNode, useState } from 'react';
 
 const HandybusGuide = () => {
+  const [currentTab, setCurrentTab] = useState<'SHUTTLE_BUS' | 'HANDY_PARTY'>(
+    'SHUTTLE_BUS',
+  );
+
+  const handleChangeTab = (value: 'SHUTTLE_BUS' | 'HANDY_PARTY') => {
+    setCurrentTab(value);
+  };
+
   return (
     <main>
       <Header />
       <figure>
         <Image src={guideImage} alt="핸디버스 가이드" quality={100} />
       </figure>
-      <article className="mx-16 mb-16 flex flex-col gap-64">
+      <article className="mx-16 mb-16 flex flex-col">
         <section className="mt-16 flex flex-col gap-8">
           <h1 className="text-22 font-700 leading-[140%]">핸디버스 가이드</h1>
           <p className="text-16 font-500 leading-[160%] text-basic-grey-700">
@@ -33,20 +45,37 @@ const HandybusGuide = () => {
             예약 과정을 알려드릴게요.
           </p>
         </section>
-        {HANDYBUS_GUIDE_STEPS.map((item) => (
-          <GuideItem key={item.order} {...item} />
-        ))}
-        <nav className="flex flex-col gap-16">
-          <h2 className="text-18 font-600 leading-[140%]">
-            핸디버스 탈 준비 되셨나요?
-          </h2>
-          <Link href="/event">
-            <Button variant="secondary">행사 둘러보기</Button>
-          </Link>
-          <Link href="/help/faq">
-            <Button variant="tertiary">자주 묻는 질문</Button>
-          </Link>
-        </nav>
+        <Tabs
+          items={
+            [
+              { label: '셔틀버스', value: 'SHUTTLE_BUS' },
+              { label: '핸디팟', value: 'HANDY_PARTY' },
+            ] as const
+          }
+          selected={currentTab}
+          onSelect={handleChangeTab}
+          className="my-48"
+        />
+        <section className="flex flex-col gap-64">
+          {currentTab === 'SHUTTLE_BUS'
+            ? HANDYBUS_GUIDE_STEPS.map((item) => (
+                <GuideItem key={item.order} {...item} />
+              ))
+            : HANDYPARTY_GUIDE_STEPS.map((item) => (
+                <GuideItem key={item.order} {...item} />
+              ))}
+          <nav className="flex flex-col gap-16">
+            <h2 className="text-18 font-600 leading-[140%]">
+              핸디버스 탈 준비 되셨나요?
+            </h2>
+            <Link href="/event">
+              <Button variant="secondary">행사 둘러보기</Button>
+            </Link>
+            <Link href="/help/faq">
+              <Button variant="tertiary">자주 묻는 질문</Button>
+            </Link>
+          </nav>
+        </section>
       </article>
     </main>
   );
@@ -57,7 +86,8 @@ export default HandybusGuide;
 interface GuideItemProps {
   order: number;
   title: string;
-  description: string;
+  description: ReactNode;
+  subDescription?: string;
   image: StaticImageData;
   tagDescription?: string;
   redirectUrl?: string;
@@ -67,6 +97,7 @@ const GuideItem = ({
   order,
   title,
   description,
+  subDescription,
   image,
   tagDescription,
   redirectUrl,
@@ -83,9 +114,16 @@ const GuideItem = ({
       <figure>
         <Image src={image} alt={title} quality={100} />
       </figure>
-      <p className="text-16 font-400 leading-[160%] text-basic-grey-700">
-        {description}
-      </p>
+      <div>
+        <p className="text-16 font-400 leading-[160%] text-basic-grey-700">
+          {description}
+        </p>
+        {subDescription && (
+          <p className="pt-4 text-14 font-500 leading-[160%] text-basic-grey-400">
+            {subDescription}
+          </p>
+        )}
+      </div>
       {redirectUrl && (
         <Link href={redirectUrl} target="_blank">
           <Button
@@ -156,6 +194,70 @@ const HANDYBUS_GUIDE_STEPS = [
     description:
       '예약하신 시간과 정류장에 맞춰 셔틀버스에 탑승하세요! 당일 교통 상황 등으로 인한 실시간 변동 사항은 오픈 채팅방을 통해 공지드릴게요.',
     image: guideImageStep6,
+    tagDescription: '탑승 당일',
+  },
+];
+
+const HANDYPARTY_GUIDE_STEPS = [
+  {
+    order: 1,
+    title: '수요조사 참여',
+    description:
+      '행사 별 셔틀이 열리도록 수요조사에 참여하세요. 원하는 지역과 정류장을 선택하고, ‘요청하기’를 누르면 참여가 완료돼요. 수요조사는 행사 15일 전에 마감되며, 기준 인원을 넘은 지역에 노선이 개설돼요.',
+    image: handypartyGuideImageStep1,
+    tagDescription: '탑승 15일 전 마감',
+  },
+  {
+    order: 2,
+    title: '셔틀 오픈 알림',
+    description:
+      '요청한 지역에 셔틀이 열리면 카카오톡으로 알림을 보내드려요. 공지사항에서도 노선에 대한 정보를 확인할 수 있어요. 인기 정류장은 빠르게 매진되니 서둘러 예약하세요!',
+    image: handypartyGuideImageStep2,
+  },
+  {
+    order: 3,
+    title: '예약하기',
+    description: (
+      <>
+        우리 동네 정류장을 골라주세요. 예를 들어, &apos;서울특별시 성북구 안암로
+        145&apos;라면 <strong>[집앞하차] 서울 성북구 정류장</strong>을 찾아
+        예약을 진행해 주세요.
+      </>
+    ),
+    image: handypartyGuideImageStep3,
+  },
+  {
+    order: 4,
+    title: '셔틀 확정 알림',
+    description:
+      '탑승일 기준 5일 전까지 운행 확정 여부를 안내드려요. 확정 시 탑승 주소 입력 링크를, 무산 시 취소 안내 및 환불이 즉시 진행되니 핸디버스의 소식을 기다려주세요!',
+    image: handypartyGuideImageStep4,
+    tagDescription: '탑승 5일 전',
+  },
+  {
+    order: 5,
+    title: '탑승 주소 입력',
+    description:
+      '운행 확정 알림과 함께 문자로 발송되는 링크에 희망하는 정확한 주소를 남겨주세요. 해당 장소에서 탑승, 하차하실 수 있어요.',
+    subDescription:
+      '* 입력하신 주소는 서비스 이용 목적 외에 절대 사용되지 않습니다.',
+    image: handypartyGuideImageStep5,
+    tagDescription: '개별 안내',
+  },
+  {
+    order: 6,
+    title: '오픈채팅방 참여',
+    description:
+      '기사님과 차량 배차가 완료되면, 오픈채팅방 링크를 전달해 드려요. 탑승 전까지 오픈채팅방에서 자유롭게 이야기를 나누어 보세요!',
+    image: handypartyGuideImageStep6,
+    tagDescription: '탑승 1일 전 초대',
+  },
+  {
+    order: 7,
+    title: '탑승',
+    description:
+      '탑승 30분 전까지 기사님의 개별 연락으로 주차 위치를 안내받을 수 있어요. 정해진 시간에 맞춰 도착하지 않거나, 연락이 두절되는 경우 탑승이 어려울 수 있으니 꼭 주의해 주세요.',
+    image: handypartyGuideImageStep7,
     tagDescription: '탑승 당일',
   },
 ];
