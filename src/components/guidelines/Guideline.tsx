4759c2f368e128a6cd9862e164d294b6b4c75e04
Geon <geonulim@gmail.com>
2025-06-05 17:37:22 +0900

    feat: 유의사항 업데이트

--- src/components/guidelines/Guideline.tsx
+++ src/components/guidelines/Guideline.tsx
@@ -1,14 +1,14 @@
 import Demand from './contents/Demand';
-import CancellationAndRefund from './contents/CancellationAndRefund';
 import Notice from './contents/Notice';
 import type { ReactNode } from 'react';
+import Inquiry from './contents/Inquiry';
 
-type Guidelines = '수요조사' | '취소 및 환불 안내' | '유의사항';
+type Guidelines = '수요조사' | '문의' | '상품별 유의사항';
 
 const GUIDELINES: Record<Guidelines, ReactNode> = {
   수요조사: <Demand />,
-  '취소 및 환불 안내': <CancellationAndRefund />,
-  유의사항: <Notice />,
+  문의: <Inquiry />,
+  '상품별 유의사항': <Notice />,
 };
 
 interface Props {
