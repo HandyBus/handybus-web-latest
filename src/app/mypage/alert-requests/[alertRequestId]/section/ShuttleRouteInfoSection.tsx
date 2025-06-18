d5e7eef5e9065ea1d7dc1a97a3d049ce73c2ea12
jujeon <jwjsjwnsstjd@gmail.com>
2025-06-18 17:40:10 +0900

    feat: 빈자리알림 상세페이지

---
+++ src/app/mypage/alert-requests/[alertRequestId]/section/ShuttleRouteInfoSection.tsx
@@ -0,0 +1,41 @@
+import { ShuttleRoutesViewEntity } from '@/types/shuttleRoute.type';
+
+interface Props {
+  shuttleRoute: ShuttleRoutesViewEntity;
+}
+
+const ShuttleRouteInfoSection = ({ shuttleRoute }: Props) => {
+  const hubName = shuttleRoute.name;
+  const toDestinationShuttleRouteHubs =
+    shuttleRoute.toDestinationShuttleRouteHubs?.map((hub) => hub.name);
+  const fromDestinationShuttleRouteHubs =
+    shuttleRoute.fromDestinationShuttleRouteHubs?.map((hub) => hub.name);
+
+  // 오는편/가는편 둘 중 하나는 반드시 존재함. 또한 hub는 미러링이 보장됨
+  const shuttleRouteHubs =
+    toDestinationShuttleRouteHubs || fromDestinationShuttleRouteHubs;
+
+  return (
+    <section className="px-16">
+      <h3 className="pb-16 text-16 font-600">수요조사 상세</h3>
+      <div className="grid grid-cols-[72px_1fr] gap-x-16 gap-y-8 text-14 font-600">
+        <h5>노선</h5>
+        <p>{shuttleRoute.name}</p>
+        <h5>정류장</h5>
+        <p>{hubName}</p>
+      </div>
+      <div className="rounded-8 bg-basic-grey-50 text-12 font-500 leading-[160%] text-basic-grey-500">
+        <h5 className="pb-4 text-14 text-basic-grey-700">노선별 정류장</h5>
+        <p className="pb-12">
+          같은 노선은 한 대의 버스가 운행해요. 빈자리가 나면 어느 정류장이든 탈
+          수 있어서 한 번만 알림 신청하면 돼요.
+        </p>
+        <p className="text-basic-grey-600">
+          [{shuttleRoute.name}] {shuttleRouteHubs?.join(' - ')}
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default ShuttleRouteInfoSection;
---
+++ src/app/mypage/alert-requests/[alertRequestId]/section/shuttle-progress-section/hooks/useText.tsx
@@ -0,0 +1,37 @@
+import { ShuttleRouteAlertRequestsViewEntity } from '@/types/alertRequest.type';
+import { useMemo } from 'react';
+
+interface Props {
+  alertRequest: ShuttleRouteAlertRequestsViewEntity;
+}
+
+const useText = ({ alertRequest }: Props) => {
+  const shuttleRoute = alertRequest.shuttleRoute;
+  const hasEmptySeat =
+    alertRequest.notifiedAt !== null && shuttleRoute.remainingSeatCount > 0;
+  const isReservationEnded = shuttleRoute.status !== 'OPEN';
+
+  const text = useMemo(() => {
+    if (isReservationEnded && !hasEmptySeat) {
+      return {
+        progressText: (
+          <span className="text-brand-primary-400">
+            예약 가능한 자리가 생겼어요
+          </span>
+        ),
+        descriptionText:
+          '빈자리가 생겼어요. 지금 바로 예약하세요! 알림 요청 여부와 상관없이 예약은 선착순으로 진행되고 있어요. 빈 자리가 사라지면 기존 대기 번호로 돌아가요.',
+      };
+    } else {
+      return {
+        progressText: `${alertRequest.shuttleRoute.remainingSeatCount}번째로 대기중이에요`,
+        descriptionText:
+          '대기 순서에 따라 알림을 보내드리고 있어요. 알림 요청 여부와 상관없이 예약은 선착순으로 진행되고 있어요.',
+      };
+    }
+  }, [alertRequest, isReservationEnded, hasEmptySeat]);
+
+  return text;
+};
+
+export default useText;
