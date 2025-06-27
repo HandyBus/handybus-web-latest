9929a72248bc373136f405b23a37dea005bdc112
jujeon <jwjsjwnsstjd@gmail.com>
2025-06-27 17:50:36 +0900

    fix: 핸디팟 정류장일때 지도 클릭시 404 페이지 이슈

--- src/app/event/[eventId]/components/shuttle-route-detail-view/components/KakaoMap.tsx
+++ src/app/event/[eventId]/components/shuttle-route-detail-view/components/KakaoMap.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { TAXI_HUB_PREFIX } from '@/utils/taxiRoute.util';
 import { useEffect, useRef } from 'react';
 
 interface Props {
@@ -51,6 +52,11 @@ const KakaoMap = ({
     window.kakao.maps.load(initializeMap);
   }, [mapRef, isKakaoMapScriptLoaded]);
 
+  // NOTE: 핸디팟인 경우 임시 처리
+  const placeNameWithException = placeName.startsWith(TAXI_HUB_PREFIX ?? '')
+    ? `핸디팟 ${placeName.split(TAXI_HUB_PREFIX ?? '')[1]} 지역`
+    : placeName;
+
   return (
     <div
       ref={mapRef}
@@ -58,7 +64,7 @@ const KakaoMap = ({
       onClick={() => {
         if (typeof window !== 'undefined') {
           window.open(
-            `https://map.kakao.com/link/map/${placeName},${latitude},${longitude}`,
+            `https://map.kakao.com/link/map/${placeNameWithException},${latitude},${longitude}`,
             '_blank',
             'noopener,noreferrer',
           );
