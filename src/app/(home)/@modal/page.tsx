4f6986ecdf5554523a47c4459e03155975ef4e81
jujeon <jwjsjwnsstjd@gmail.com>
2025-08-07 20:30:41 +0900

    hotfix: 긴급 모달 이미지 변경

--- src/app/(home)/@modal/page.tsx
+++ src/app/(home)/@modal/page.tsx
@@ -4,7 +4,7 @@ import ModalPortal from '@/components/modals/ModalPortal';
 // import OneDayModal from './components/OneDayModal';
 // import couponEventImage from './images/coupon-event.png';
 import ArgentModal from './components/ArgentModal';
-import argentEventImage from './images/argent-modal.png';
+import argentEventImage from './images/argent-modal2.png';
 
 const Page = () => {
   return (
