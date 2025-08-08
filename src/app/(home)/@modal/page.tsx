9ea45543a40c1d47a899d542d1ebedfdc866e4fa
jujeon <jwjsjwnsstjd@gmail.com>
2025-08-08 11:35:51 +0900

    hotfix: 긴급모달 내리기

--- src/app/(home)/@modal/page.tsx
+++ src/app/(home)/@modal/page.tsx
@@ -1,16 +1,13 @@
 'use client';
 
 import ModalPortal from '@/components/modals/ModalPortal';
-// import OneDayModal from './components/OneDayModal';
-// import couponEventImage from './images/coupon-event.png';
-import ArgentModal from './components/ArgentModal';
-import argentEventImage from './images/argent-modal2.png';
+import OneDayModal from './components/OneDayModal';
+import couponEventImage from './images/coupon-event.png';
 
 const Page = () => {
   return (
     <ModalPortal>
-      {/* <OneDayModal image={couponEventImage} href="/demand-reward-coupon" /> */}
-      <ArgentModal image={argentEventImage} />
+      <OneDayModal image={couponEventImage} href="/demand-reward-coupon" />
     </ModalPortal>
   );
 };
