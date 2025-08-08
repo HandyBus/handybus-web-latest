69e7c49987ab3d984e24151a2e5bbf192925394c
jujeon <jwjsjwnsstjd@gmail.com>
2025-08-08 12:22:25 +0900

    hotfix: 긴급모달 다시 올리기

--- src/app/(home)/@modal/page.tsx
+++ src/app/(home)/@modal/page.tsx
@@ -1,13 +1,16 @@
 'use client';
 
 import ModalPortal from '@/components/modals/ModalPortal';
-import OneDayModal from './components/OneDayModal';
-import couponEventImage from './images/coupon-event.png';
+// import OneDayModal from './components/OneDayModal';
+// import couponEventImage from './images/coupon-event.png';
+import argentModalImage from './images/argent-modal2.png';
+import ArgentModal from './components/ArgentModal';
 
 const Page = () => {
   return (
     <ModalPortal>
-      <OneDayModal image={couponEventImage} href="/demand-reward-coupon" />
+      {/* <OneDayModal image={couponEventImage} href="/demand-reward-coupon" /> */}
+      <ArgentModal image={argentModalImage} href="/demand-reward-coupon" />
     </ModalPortal>
   );
 };
