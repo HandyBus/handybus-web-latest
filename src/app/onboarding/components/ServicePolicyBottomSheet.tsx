4962ad4c8d60313624a72d8b8b6805f82ca752f4
Geon <geonulim@gmail.com>
2025-08-14 11:25:24 +0900

    feat: 온보딩 퍼널에 약관 추가

---
+++ src/app/onboarding/components/ServicePolicyBottomSheet.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import BottomSheet from '@/components/bottom-sheet/BottomSheet';
+import { policies } from '@/data/policy';
+import { BottomSheetRefs } from '@/hooks/useBottomSheet';
+
+const ServicePolicyBottomSheet = ({
+  bottomSheetRef,
+  contentRef,
+}: BottomSheetRefs) => {
+  return (
+    <BottomSheet title="서비스 이용 약관" ref={bottomSheetRef}>
+      <div ref={contentRef} className="overflow-y-auto">
+        {policies['서비스이용약관']}
+      </div>
+    </BottomSheet>
+  );
+};
+
+export default ServicePolicyBottomSheet;
