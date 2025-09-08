410a688e50a052b212fcf352b9002862fa6283b5
jujeon <jwjsjwnsstjd@gmail.com>
2025-09-08 16:07:28 +0900

    fix: 콘서트행으로 잘 못 표기되어있던 용어 수정

--- src/constants/status.ts
+++ src/constants/status.ts
@@ -2,7 +2,7 @@ import { EventType } from '@/types/event.type';
 import { TripType } from '@/types/shuttleRoute.type';
 
 export const TRIP_STATUS_TO_STRING: Record<TripType, string> = {
-  TO_DESTINATION: '콘서트행',
+  TO_DESTINATION: '행사장행',
   FROM_DESTINATION: '귀가행',
   ROUND_TRIP: '왕복',
 } as const;
