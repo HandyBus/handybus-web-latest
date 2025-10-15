f3eb51e2e9e0638005fb3d49aea2af059b0e471e
Geon <geonulim@gmail.com>
2025-10-15 17:38:11 +0900

    fix: 취소된 예약에 대해 탑승 관련 안내 숨김 처리

--- src/app/history/reservation/[reservationId]/components/sections/title-section/TitleSection.tsx
+++ src/app/history/reservation/[reservationId]/components/sections/title-section/TitleSection.tsx
@@ -13,6 +13,7 @@ interface Props {
 }
 
 const TitleSection = ({ event, reservation, shuttleRoute }: Props) => {
+  const isReservationCancelled = reservation.reservationStatus === 'CANCEL';
   return (
     <>
       <Title
@@ -20,10 +21,12 @@ const TitleSection = ({ event, reservation, shuttleRoute }: Props) => {
         shuttleRouteStatus={shuttleRoute.status}
       />
       <EventCard event={event} />
-      <div className="mx-16 mb-24 h-[38px] rounded-6 bg-basic-grey-50 px-16 py-8 text-center text-14 font-500 leading-[160%]">
-        최종 탑승 정보는 탑승{' '}
-        <span className="text-brand-primary-400">1일 전</span> 알려드려요.
-      </div>
+      {!isReservationCancelled && (
+        <div className="mx-16 mb-24 h-[38px] rounded-6 bg-basic-grey-50 px-16 py-8 text-center text-14 font-500 leading-[160%]">
+          최종 탑승 정보는 탑승{' '}
+          <span className="text-brand-primary-400">1일 전</span> 알려드려요.
+        </div>
+      )}
     </>
   );
 };
