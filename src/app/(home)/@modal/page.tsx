a19e1b9aea5605c9c3a09f1ca0d377eb026dbb1b
jujeon <jwjsjwnsstjd@gmail.com>
2025-10-16 16:19:17 +0900

    hotfix: 오아시스 모달 내림

--- src/app/(home)/@modal/page.tsx
+++ src/app/(home)/@modal/page.tsx
@@ -3,11 +3,11 @@
 import ModalPortal from '@/components/modals/ModalPortal';
 import argentModalImage from './images/argent-modal2.png';
 import ArgentModal from './components/ArgentModal';
-import OneDayModal from './components/OneDayModal';
-import OasisHandyPartyImage from './images/oasis-handyparty-modal.png';
+// import OneDayModal from './components/OneDayModal';
+// import OasisHandyPartyImage from './images/oasis-handyparty-modal.png';
 
-const OASIS_HANDYPARTY_MODAL_URL =
-  'https://www.handybus.co.kr/announcements/633200611310965713';
+// const OASIS_HANDYPARTY_MODAL_URL =
+//   'https://www.handybus.co.kr/announcements/633200611310965713';
 
 const Page = () => {
   const showEmergencyModal =
@@ -15,10 +15,10 @@ const Page = () => {
   return (
     <ModalPortal>
       {showEmergencyModal && <ArgentModal image={argentModalImage} />}
-      <OneDayModal
+      {/* <OneDayModal
         image={OasisHandyPartyImage}
         href={OASIS_HANDYPARTY_MODAL_URL}
-      />
+      /> */}
       {/* <EventPromotionModal
         image={EventPromotionModalImage}
         href={EVENT_PROMOTION_MODAL_URL}
