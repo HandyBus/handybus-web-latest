96d9a750dea3f94aa954e646b38340c927ca5c82
Geon <geonulim@gmail.com>
2025-10-27 18:05:51 +0900

    feat: url 변경 전 주소들 redirection 처리

--- src/app/mypage/shuttle/demand/[demandId]/page.tsx
+++ src/app/mypage/shuttle/demand/[demandId]/page.tsx
@@ -1,11 +1,7 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import DeferredSuspense from '@/components/loading/DeferredSuspense';
-import Loading from '@/components/loading/Loading';
-import Header from '@/components/header/Header';
-import { useGetUserDemand } from '@/services/demand.service';
-import Content from './components/Content';
+import { useEffect } from 'react';
 
 interface Props {
   params: {
@@ -16,24 +12,10 @@ interface Props {
 const Page = ({ params }: Props) => {
   const { demandId } = params;
   const router = useRouter();
-  const { data: demand, isLoading, isSuccess } = useGetUserDemand(demandId);
-
-  if (isSuccess && !demand) {
-    router.replace('/mypage/shuttle?type=demand');
-    return <div className="h-[100dvh]" />;
-  }
-
-  return (
-    <>
-      <Header />
-      <DeferredSuspense
-        fallback={<Loading style="grow" />}
-        isLoading={isLoading}
-      >
-        {demand && <Content demand={demand} />}
-      </DeferredSuspense>
-    </>
-  );
+  useEffect(() => {
+    router.replace(`/history/demand/${demandId}`);
+  }, [router, demandId]);
+  return null;
 };
 
 export default Page;
