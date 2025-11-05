cb104a621d06550d8ea163d77811d400b0753e03
Geon <geonulim@gmail.com>
2025-11-05 14:03:25 +0900

    feat: 신규 페이지 메타데이터 적용

--- src/app/event/layout.tsx
+++ src/app/event/layout.tsx
@@ -1,11 +1,12 @@
 import { Metadata } from 'next';
+import { ReactNode } from 'react';
 
 export const metadata: Metadata = {
   title: '진행 중인 행사 전체보기',
   description: '진행 중인 행사 셔틀을 확인해보세요!',
 };
 
-const EventLayout = ({ children }: { children: React.ReactNode }) => {
+const EventLayout = ({ children }: { children: ReactNode }) => {
   return <>{children}</>;
 };
 
