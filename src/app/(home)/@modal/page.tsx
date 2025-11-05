a1aa95c61743c195f20d7a2db0716c5c4454f16e
Geon <geonulim@gmail.com>
2025-11-05 13:22:31 +0900

    feat: 앱 출시 이벤트 쿠폰 다운로드 모달

--- src/app/(home)/@modal/page.tsx
+++ src/app/(home)/@modal/page.tsx
@@ -8,6 +8,13 @@ import { useRouter } from 'next/navigation';
 import useEnvironment from '@/hooks/useEnvironment';
 // import OneDayModal from './components/OneDayModal';
 import EventPromotionModal from './components/EventPromotionModal';
+import { getIsLoggedIn } from '@/utils/handleToken.util';
+import {
+  getAppLaunchEventCouponDownloadModalSeen,
+  setAppLaunchEventCouponDownloadModalSeen,
+} from '@/utils/localStorage';
+import AppLaunchEventCouponDownloadModal from './components/AppLaunchEventCouponDownloadModal';
+import { useEffect, useState } from 'react';
 
 const Page = () => {
   const router = useRouter();
@@ -18,22 +25,50 @@ const Page = () => {
     process.env.NEXT_PUBLIC_ENABLE_EMERGENCY_MODAL === 'true';
 
   const { isApp } = useEnvironment();
+  const isLoggedIn = getIsLoggedIn();
+
   const showAppLaunchEventModal = !isApp;
 
+  const [
+    isAppLaunchEventCouponDownloadModalOpen,
+    setIsAppLaunchEventCouponDownloadModalOpen,
+  ] = useState(false);
+
+  const closeAppLaunchEventCouponDownloadModal = () => {
+    setIsAppLaunchEventCouponDownloadModalOpen(false);
+    setAppLaunchEventCouponDownloadModalSeen();
+  };
+
+  useEffect(() => {
+    const isAppLaunchEventCouponDownloadModalSeen =
+      getAppLaunchEventCouponDownloadModalSeen();
+    const showAppLaunchEventCouponDownloadModal =
+      isApp && isLoggedIn && !isAppLaunchEventCouponDownloadModalSeen;
+    if (showAppLaunchEventCouponDownloadModal) {
+      setIsAppLaunchEventCouponDownloadModalOpen(true);
+    }
+  }, []);
+
   return (
-    <ModalPortal>
-      {showEmergencyModal && <ArgentModal image={argentModalImage} />}
-      {/* <OneDayModal
+    <>
+      <ModalPortal>
+        {showEmergencyModal && <ArgentModal image={argentModalImage} />}
+        {/* <OneDayModal
           image={AppLaunchEventImage}
           handleClick={handleAppLaunchEventOpen}
         /> */}
-      {showAppLaunchEventModal && (
-        <EventPromotionModal
-          image={AppLaunchEventImage}
-          handleClick={handleAppLaunchEventOpen}
-        />
-      )}
-    </ModalPortal>
+        {showAppLaunchEventModal && (
+          <EventPromotionModal
+            image={AppLaunchEventImage}
+            handleClick={handleAppLaunchEventOpen}
+          />
+        )}
+      </ModalPortal>
+      <AppLaunchEventCouponDownloadModal
+        isOpen={isAppLaunchEventCouponDownloadModalOpen}
+        closeModal={closeAppLaunchEventCouponDownloadModal}
+      />
+    </>
   );
 };
 
