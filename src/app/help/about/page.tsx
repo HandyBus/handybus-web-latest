c5b2fd5695d4a94e7b096bf6b622affc3d829c58
jujeon <jwjsjwnsstjd@gmail.com>
2025-11-07 10:51:43 +0900

    design: 서비스 소개

--- src/app/help/about/page.tsx
+++ src/app/help/about/page.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import OverviewImage1 from './images/service-overview-1.png';
 import OverviewImage2 from './images/service-overview-2.png';
 import Link from 'next/link';
+import RequestEventButton from './components/RequestEventButton';
 
 export const metadata: Metadata = {
   title: '소개',
@@ -14,67 +15,31 @@ const AboutPage = () => {
     <>
       <Header />
       <main>
-        <ServiceOverview />
-        <FAQTop5 />
+        <Image src={OverviewImage1} alt="overview" />
+        <div className="flex flex-col items-center gap-8 pb-32">
+          <Link
+            href="/event"
+            className="h-[50px] w-[calc(100%-64px)] rounded-[8px] bg-brand-primary-400 px-16 py-12 text-center text-16 font-600 leading-[160%] text-basic-white"
+          >
+            셔틀 수요조사 참여하기
+          </Link>
+          <RequestEventButton>행사 요청하기</RequestEventButton>
+        </div>
+        <Image src={OverviewImage2} alt="overview" />
+        <div className="flex flex-col items-center gap-16 bg-basic-black pb-48 pt-32">
+          <p className="text-20 font-600 leading-[140%] text-basic-white">
+            설레는 여정을 함께 떠나볼까요?
+          </p>
+          <Link
+            href="/event"
+            className="h-[50px] w-[calc(100%-64px)] rounded-[8px] bg-brand-primary-400 px-16 py-12 text-center text-16 font-600 leading-[160%] text-basic-white"
+          >
+            행사 둘러보기
+          </Link>
+        </div>
       </main>
     </>
   );
 };
 
 export default AboutPage;
-
-const ServiceOverview = () => {
-  return (
-    <>
-      <Image src={OverviewImage1} alt="overview" />
-      <section className="relative h-0 w-full bg-brand-primary-50">
-        <Link
-          href="/help/handybus-guide"
-          className="relative -top-100 mx-32 block h-[46px] w-[calc(100%-64px)] cursor-pointer bg-transparent"
-        />
-      </section>
-      <Image src={OverviewImage2} alt="overview" />
-    </>
-  );
-};
-
-import ArrowForwardIcon from './icons/arrow-forward.svg';
-import { faqs } from '@/data/faq';
-import Accordion from '@/components/accordion/Accordion';
-
-const FAQTop5 = () => {
-  return (
-    <section className="flex flex-col px-16 py-24">
-      <h2 className="pb-[40px] text-center text-20 font-600 leading-[140%]">
-        자주 묻는 질문
-      </h2>
-      <section className="pb-16">
-        {faqs.slice(0, 5).map((item) => {
-          const Content = item.content;
-          return (
-            <Accordion
-              key={item.title}
-              title={
-                <span className="text-16 font-600 leading-[160%] text-basic-black">
-                  {item.title}
-                </span>
-              }
-              containerClassName="px-8"
-              titleClassName="py-12"
-            >
-              <div className="whitespace-pre-line pb-8 text-14 font-500 leading-[160%] text-basic-grey-600 [&>summary>h3]:text-16 [&>summary>h3]:font-600 [&>summary>h3]:leading-[160%] [&>summary>h3]:text-basic-black [&_li]:ml-16 [&_ol>li]:ml-16 [&_ol>li]:whitespace-normal [&_ol>li]:text-16 [&_ol>li]:font-600 [&_ol]:list-decimal [&_ol]:whitespace-normal [&_strong]:text-basic-grey-600 [&_ul>li]:whitespace-normal [&_ul>li]:text-16 [&_ul]:list-disc [&_ul]:whitespace-normal">
-                {Content}
-              </div>
-            </Accordion>
-          );
-        })}
-      </section>
-      <Link
-        href="/help/faq"
-        className="flex h-[46px] items-center justify-center gap-[10px] rounded-[8px] text-16 font-600 leading-[160%] text-basic-grey-700"
-      >
-        더 많은 질문 보기 <ArrowForwardIcon />
-      </Link>
-    </section>
-  );
-};
