3d6047dfab238d345591eabd32f367e917c7a0d4
Geon <geonulim@gmail.com>
2025-11-21 15:24:45 +0900

    fix: activity name을 통해 헤더 페이지 이름 표시

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,48 +1,30 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
 import LogoIcon from 'public/icons/logo-v3.svg';
 import BackIcon from './icons/back.svg';
 import HomeIcon from './icons/home.svg';
 import AnnouncementsIcon from './icons/announcement.svg';
-import { useFlow } from '@/stacks';
+import { STACK_NAME_TO_PAGE_NAME, StackName, useFlow } from '@/stacks';
 import { useActivity } from '@stackflow/react';
 import usePopAll from '@/hooks/usePopAll';
+import { useState, useEffect } from 'react';
 
 const Header = () => {
   // 경로에 따른 페이지명 표시
   const flow = useFlow();
-  const pathname = usePathname();
-  const isHome = pathname === '/';
-
-  const isHideBackButton = PATHNAME_TO_HIDE_BACK_BUTTON.includes(pathname);
-
-  const normalizePath = (path: string) => {
-    return path
-      .split('/')
-      .map((segment) => {
-        // 순수 숫자 패턴
-        const isNumericId = /^\d+$/.test(segment);
-        // UUID 패턴
-        const isUuidId =
-          /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(
-            segment,
-          );
-
-        return isNumericId || isUuidId ? ':id' : segment;
-      })
-      .join('/');
-  };
-
-  const getPageName = (path: string) => {
-    const normalizedPath = normalizePath(path);
-    return (
-      URL_TO_PAGE_NAME?.[normalizedPath as keyof typeof URL_TO_PAGE_NAME] || ''
-    );
-  };
-  const pageName = getPageName(pathname);
-
   const activity = useActivity();
+  const [pageName, setPageName] = useState('');
+
+  useEffect(() => {
+    setPageName(STACK_NAME_TO_PAGE_NAME[activity.name as StackName] || '');
+  }, [activity.name]);
+
+  const isHome = activity.name === 'Home';
+
+  const isHideBackButton = STACK_NAME_TO_HIDE_BACK_BUTTON.includes(
+    activity.name,
+  );
+
   const popAll = usePopAll();
   const handleBackClick = () => {
     const isRoot = activity.isRoot;
@@ -97,50 +79,10 @@ const Header = () => {
 
 export default Header;
 
-// 새로운 페이지 개발 시 이곳에 페이지명을 추가해주세요.
-const URL_TO_PAGE_NAME = {
-  '/login': '로그인',
-  '/event': '모든 행사',
-  '/event-from-home': '모든 행사',
-  '/event/:id': '행사 정보',
-  '/event/:id/dailyevent/:id/route/:id/payment/request': '결제',
-  '/event/:id/dailyevent/:id/route/:id/payment/request/:id': '결제',
-  '/event/:id/dailyevent/:id/route/:id/payment/request/fail': '결제',
-  '/ticket': '모든 탑승권',
-  '/ticket/:id': '탑승권',
-  '/history': '참여/예약 내역',
-  '/history/reservation/:id': '예약 정보',
-  '/history/reservation/:id/reservation-transfer': '선물하기',
-  '/history/reservation/:id/reservation-transfer/success': '선물 완료',
-  '/history/demand/:id': '수요조사 정보',
-  '/mypage': '마이페이지',
-  '/mypage/profile/edit': '프로필 수정',
-  '/mypage/settings': '환경설정',
-  '/mypage/coupons': '쿠폰',
-  '/mypage/reviews': '내 후기',
-  '/mypage/reviews/write/:id': '후기 작성',
-  '/mypage/alert-requests': '빈자리 알림',
-  '/mypage/alert-requests/:id': '빈자리 알림 정보',
-  '/mypage/boarding-pass/:id': '탑승권',
-  '/announcements': '공지사항',
-  '/announcements/:id': '공지글',
-  '/help/about': '서비스 소개',
-  '/help/handybus-guide': '이용 방법',
-  '/help/faq': '도움말',
-  '/help/faq/direct-inquiry': '직접 문의하기',
-  '/help/faq/terms-of-service': '서비스 이용 약관',
-  '/help/faq/privacy-policy': '개인정보 처리 방침',
-  '/help/faq/marketing-consent': '마케팅 활용 동의',
-  '/accept-reservation-transfer/:id': '탑승권 선물',
-  '/accept-reservation-transfer/:id/success': '선물 완료',
-  '/accept-reservation-transfer/:id/fail': '탑승권 선물',
-  '/app-launch-event': '이벤트',
-};
-
-const PATHNAME_TO_HIDE_BACK_BUTTON = [
-  '/',
-  '/event',
-  '/history',
-  '/mypage',
-  '/ticket',
+const STACK_NAME_TO_HIDE_BACK_BUTTON = [
+  'Home',
+  'EventList',
+  'History',
+  'MyPage',
+  'Ticket',
 ];
