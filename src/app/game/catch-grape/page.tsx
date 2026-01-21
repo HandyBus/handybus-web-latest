d4c7ca0b7e014247467dde463bddae8b5dcf28bc
jujeon <jwjsjwnsstjd@gmail.com>
2026-01-21 15:48:25 +0900

    feat: 메타데이터 등록

--- src/app/game/catch-grape/page.tsx
+++ src/app/game/catch-grape/page.tsx
@@ -1,4 +1,22 @@
+import { Metadata } from 'next';
 import CatchGrapeGame from './components/CatchGrapeGame';
+
+export const metadata: Metadata = {
+  title: '핸디버스 | 포도알 트레이닝',
+  description: '이번 티켓팅, 포도알 트레이닝으로 맹연습해서 같이 성공할까요?',
+  openGraph: {
+    title: '핸디버스 | 포도알 트레이닝',
+    description: '이번 티켓팅, 포도알 트레이닝으로 맹연습해서 같이 성공할까요?',
+    images: [
+      {
+        url: '/src/app/game/catch-grape/images/thumbnail.png',
+        width: 1200,
+        height: 630,
+        alt: '포도알 트레이닝',
+      },
+    ],
+  },
+};
 
 const CatchGrapePage = () => {
   return <CatchGrapeGame />;
