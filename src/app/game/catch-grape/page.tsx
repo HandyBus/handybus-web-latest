3f92b3f3e5d3659fc1192e4f7b8892d5d1a613bd
jujeon <jwjsjwnsstjd@gmail.com>
2026-01-21 16:08:23 +0900

    fix: metadata title

--- src/app/game/catch-grape/page.tsx
+++ src/app/game/catch-grape/page.tsx
@@ -3,7 +3,7 @@ import { URL } from '@/constants/metadata';
 import CatchGrapeGame from './components/CatchGrapeGame';
 
 export const metadata: Metadata = {
-  title: '핸디버스 | 포도알 트레이닝',
+  title: '포도알 트레이닝',
   description: '이번 티켓팅, 포도알 트레이닝으로 맹연습해서 같이 성공할까요?',
   openGraph: {
     title: '핸디버스 | 포도알 트레이닝',
